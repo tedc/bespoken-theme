de5d5fb8cdce8986a6a533b7e68d2373b5484da1
Mattia <guzzi.mattia@gmail.com>
2017-01-13 12:07:34 +0100

    no message

--- extras/setup.php
+++ extras/setup.php
@@ -2,6 +2,10 @@
 
 if (function_exists('acf_add_options_page')) {
 
-    acf_add_options_sub_page();
-    
+    acf_add_options_sub_page(array(
+        'page_title' => 'Extra',
+        'menu_title' => 'Impostaioni extra del tema',
+        'parent_slug' => 'themes.php'
+    ));
+
 }
