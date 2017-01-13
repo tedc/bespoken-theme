93535405054fb41444141a82ef94ee373812287c
Mattia <guzzi.mattia@gmail.com>
2017-01-13 12:00:44 +0100

    no message

--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,8 @@
 <footer class="content-info">
   <div class="container">
     <?php dynamic_sidebar('sidebar-footer'); ?>
+    <section class="sub-footer">
+      prova
+    </section>
   </div>
 </footer>
