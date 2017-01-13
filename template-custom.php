06f1ea2cd4c70a5a1a8d32c88befe8de8012d5f6
Mattia <guzzi.mattia@gmail.com>
2017-01-13 12:39:49 +0100

    no message

--- template-custom.php
+++ template-custom.php
@@ -7,5 +7,4 @@
 <?php while (have_posts()) : the_post(); ?>
   <?php get_template_part('templates/page', 'header'); ?>
   <?php get_template_part('templates/content', 'page'); ?>
-  <?php get_template_part('templates', 'footer'); ?>
 <?php endwhile; ?>
