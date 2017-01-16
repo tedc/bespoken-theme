2250434338dca989fc61a171bc3c63750dfe46f1
Mattia <guzzi.mattia@gmail.com>
2017-01-16 17:23:48 +0100

    no message

--- templates/header.php
+++ templates/header.php
@@ -1,14 +1,13 @@
-  <a class="logo icon-logo" href="<?= esc_url(home_url('/')); ?>">
-  </a>
-
-    <?php wp_nav_menu( ['theme_location' => 'primary_navigation', 'menu_class' => 'nav', 'container_id' => 'menu', 'container_class' => 'menu'] ); ?>
-    <a class="btn-header btn-menu" href="#" data-reveal="#menu">
-      <span class="btn-header-text"><?php _e('Menu', 'bspkn'); ?></span>
+<a class="logo icon-logo" href="<?= esc_url(home_url('/')); ?>">
+</a>
+<?php wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav', 'container_id' => 'menu', 'container_class' => 'menu']); ?>
+<a class="btn-header btn-menu" href="#" data-reveal="#menu">
+    <span class="btn-header-text"><?php _e('Menu', 'bspkn'); ?></span>
 			<span class="toggle" data-close="<?php _e('Chiudi', 'bspkn'); ?>">
 				<span class="toggle-line"></span>
 				<span class="toggle-line"></span>
 				<span class="toggle-line"></span>
 			</span>
-    </a>
-    <?php /*icl_selector(); */?>
+</a>
+<?php /*icl_selector(); */ ?>
 
