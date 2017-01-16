65351a9f2508d49f3ff677778f2ce8ef3cde0090
Mattia <guzzi.mattia@gmail.com>
2017-01-16 18:04:14 +0100

    no message

--- templates/page-header.php
+++ templates/page-header.php
@@ -1,9 +1,13 @@
 <?php use Roots\Sage\Titles; ?>
 <?php get_template_part('templates/breadcrumbs'); ?>
 <div class="page-header">
-  <?php get_template_part( 'templates/header', get_field('kind'))?>
-  <h1 class="title"><?= Titles\title(); ?></h1>
-  <?php if(get_field('sottotitolo')) : ?>
-    <h2 class="subtitle"><?php the_field('sottotitolo'); ?></h2>
-  <?php endif; ?>
+    <?php get_template_part('templates/header', get_field('kind')) ?>
+    <div class="container-text-header">
+        <div class="text-header">
+            <h1 class="title"><?= Titles\title(); ?></h1>
+            <?php if (get_field('sottotitolo')) : ?>
+                <h2 class="subtitle"><?php the_field('sottotitolo'); ?></h2>
+            <?php endif; ?>
+        </div>
+    </div>
 </div>
