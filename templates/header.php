fdc842b2ff7fb75f7f1a1b163af64b4cc41b5c30
Mattia <guzzi.mattia@gmail.com>
2017-01-16 14:36:26 +0100

    no message

--- templates/header.php
+++ templates/header.php
@@ -1,12 +1,17 @@
+<header class="total-header">
 <header class="banner">
-  <div class="container">
-    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-    <nav class="nav-primary">
-      <?php
-      if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
-      endif;
-      ?>
-    </nav>
+  <a class="logo icon-logo" href="<?= esc_url(home_url('/')); ?>">
+  </a>
+  <div class="menu-container">
+    <?php wp_nav_menu( ['theme_location' => 'primary_navigation', 'menu_class' => 'nav', 'container_id' => 'menu', 'container_class' => 'menu'] ); ?>
+    <a class="btn-header btn-menu" href="#" data-reveal="#menu">
+      <span class="btn-header-text"><?php _e('Menu', 'bspkn'); ?></span>
+			<span class="toggle" data-close="<?php _e('Chiudi', 'bspkn'); ?>">
+				<span class="toggle-line"></span>
+				<span class="toggle-line"></span>
+				<span class="toggle-line"></span>
+			</span>
+    </a>
+    <?php icl_selector(); ?>
   </div>
 </header>
