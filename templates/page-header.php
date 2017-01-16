08a5113b7933d496fecdc8b117835d0d94d44ace
Mattia <guzzi.mattia@gmail.com>
2017-01-16 17:51:21 +0100

    no message

--- templates/page-header.php
+++ templates/page-header.php
@@ -2,7 +2,7 @@
 <?php get_template_part('templates/breadcrumbs'); ?>
 <div class="page-header">
   <?php get_template_part( 'templates/header', get_field('kind'))?>
-  <h1><?= Titles\title(); ?></h1>
+  <h1 class="title"><?= Titles\title(); ?></h1>
   <?php if(get_field('sottotitolo')) : ?>
     <h2 class="subtitle"><?php the_field('sottotitolo'); ?></h2>
   <?php endif; ?>
