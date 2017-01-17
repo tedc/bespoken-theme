be1c7ff5b8030fc616b2843526606ab7955d4c0c
Mattia <guzzi.mattia@gmail.com>
2017-01-17 16:39:45 +0100

    no message

--- builder/contenuto/testo.php
+++ builder/contenuto/testo.php
@@ -1,4 +1,5 @@
 <?php while ( have_rows('testo') ) : the_row(); ?>
+    <?php var_dump(get_sub_field('allineamento_testo')) ?>
     <?php $align = ((get_sub_field('allineamento_testo') === 'Right') ? 'align-right' : (get_sub_field('allineamento_testo') === 'Center') ? 'align-center' : 'align-left'); ?>
     <h3 class="title <?php echo $align ?>"><?php echo $align ?></h3>
 <div class="description <?php echo $align ?>"><?php echo get_sub_field('contenuto') ?></div>
