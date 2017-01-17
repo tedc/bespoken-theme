a4d0ff65b118113f0f6e034f0c350fd03507418b
Mattia <guzzi.mattia@gmail.com>
2017-01-17 17:58:19 +0100

    no message

--- templates/footer.php
+++ templates/footer.php
@@ -1,7 +1,7 @@
 <footer class="content-info">
   <div class="container">
     <?php dynamic_sidebar('sidebar-footer'); ?>
-    <a class="link-cookie" href="<?php the_field('link_page_cookie', 'option'); ?>">_e('Cookie e Privacy Policy', 'bspkn')</a>
+    <a class="link-cookie" href="<?php the_field('link_page_cookie', 'option'); ?>"><?php _e('Cookie e Privacy Policy', 'bspkn')?></a>
   </div>
     <section class="sub-footer">
     </section>
