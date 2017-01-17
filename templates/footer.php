e9c99a46e043e6224ee7270c641c0a9b27a20b74
Mattia <guzzi.mattia@gmail.com>
2017-01-17 18:29:01 +0100

    no message

--- templates/footer.php
+++ templates/footer.php
@@ -4,13 +4,15 @@
         <a class="link-cookie"
            href="<?php the_field('link_page_cookie', 'option'); ?>"><?php _e('Cookie e Privacy Policy', 'bspkn') ?></a>
     </section>
-    <section class="sub-footer">
+    <section class="sub-footer bg-gradient">
         <span class="sub-footer-text"><?php the_field('frase_sub_footer', 'option'); ?></span>
         <div class="btn">
-            <span class="btn-line">
-                <span class="btn-arrow-up"></span>
-                <span class="btn-arrow-down"></span>
-            </span>
+            <a href="<?php the_field('contact_page', 'option'); ?>">
+                <span class="btn-line">
+                    <span class="btn-arrow-up"></span>
+                    <span class="btn-arrow-down"></span>
+                </span>
+            </a>
         </div>
     </section>
 </footer>
