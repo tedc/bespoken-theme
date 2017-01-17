63f5ae6964b681f702120fbc3325f797fd5b7e0e
Mattia <guzzi.mattia@gmail.com>
2017-01-17 10:40:53 +0100

    no message

--- builder/colonna.php
+++ builder/colonna.php
@@ -1,5 +1,5 @@
 <?php while (have_rows('colonna')) : the_row(); ?>
     <?php while (have_rows('contenuto')) : the_row(); ?>
-    <div class="prova"></div>
+
     <?php endwhile ?>
 <?php endwhile; ?>
