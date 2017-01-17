703685254ba153a05311c4f1d5af2538355e883a
Mattia <guzzi.mattia@gmail.com>
2017-01-17 11:42:11 +0100

    no message

--- builder/colonna.php
+++ builder/colonna.php
@@ -1,6 +1,8 @@
-<?php while (have_rows('colonna')) : the_row(); ?>
-    <?php while (have_rows('contenuto')) : the_row(); ?>
+<?php /*while (have_rows('colonna')) : the_row(); */?><!--
+    <?php /*while (have_rows('contenuto')) : the_row(); */?>
         <div class="prova"></div>
 
-    <?php endwhile ?>
-<?php endwhile; ?>
+    <?php /*endwhile */?>
+--><?php /*endwhile; */?>
+
+<div class="colonna"></div>
