17ceb763174c1aee92caa5214677ccde9b08860a
Mattia <guzzi.mattia@gmail.com>
2017-01-17 15:49:14 +0100

    no message

--- builder/contenuto/testo.php
+++ builder/contenuto/testo.php
@@ -1,2 +1,2 @@
-<h3 class="title"><?php if(get_sub_field('titolo') != '') : the_sub_field('titolo'); endif ?></h3>
-<div class="description"><?php the_sub_field('contenuto') ?></div>
+<h3 class="title"><?php if(get_sub_field('titolo') != '') : echo get_sub_field('titolo'); endif ?></h3>
+<div class="description"><?php echo get_sub_field('contenuto') ?></div>
