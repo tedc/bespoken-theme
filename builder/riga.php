07592a24e97f187faed4007de42b36240ae58a32
Mattia <guzzi.mattia@gmail.com>
2017-01-17 11:10:49 +0100

    no message

--- builder/riga.php
+++ builder/riga.php
@@ -1,11 +1,15 @@
-<?php $nCols = get_sub_field('n_cols'); ?>
 <div class="grid-12">
-    <?php while(have_rows('riga')) : the_row(); ?>
+    <?php while (have_rows('riga')) : the_row(); ?>
         <div class="grid-2">
-        <?php while(have_rows('colonna')) : the_row(); ?>
-            <?php echo get_sub_field('n_cols');?>
-            <?php include(locate_template( get_row_layout().'.php', false, false)); ?>
-        <?php endwhile; ?>
+            <?php var_dump(get_row_layout() == 'colonna') ?>
+            <?php if (get_row_layout() == 'colonna'): ?>
+
+               <!-- <?php /*while (have_rows('colonna')) : the_row(); */?>
+                    <?php /*echo get_sub_field('n_cols'); */?>
+                    <?php /*include(locate_template(get_row_layout() . '.php', false, false)); */?>
+                --><?php /*endwhile; */?>
+
+            <?php endif ?>
         </div>
     <?php endwhile; ?>
 </div>
