c0a73fd3cc6781255956db422f43fa7f833b065a
Mattia <guzzi.mattia@gmail.com>
2017-01-17 11:53:33 +0100

    no message

--- builder/colonna.php
+++ builder/colonna.php
@@ -1,8 +1,16 @@
-<?php /*while (have_rows('colonna')) : the_row(); */?><!--
-    <?php /*while (have_rows('contenuto')) : the_row(); */?>
-        <div class="prova"></div>
-
-    <?php /*endwhile */?>
---><?php /*endwhile; */?>
-
-<div class="colonna"></div>
+<?php $dim == get_sub_field('dim') ?>
+<?php switch ($dim) {
+    case '1':
+        $perc = (100);
+        break;
+    case '1/2':
+        $perc = (100 / 2);
+        break;
+    case '1/3':
+        $perc = ((100 / 3) * 1);
+        break;
+    case '2/3':
+        $perc = ((100 / 3) * 2);
+        break;
+} ?>
+<?php echo $perc ?>
--- builder/riga.php
+++ builder/riga.php
@@ -1,10 +1,6 @@
 
 <div class="grid-12">
-
-    <?php var_dump(the_sub_field('n_cols'))?>
     <?php while(have_rows('colonna')) : the_row(); ?>
-        <div></div>
         <?php include(locate_template('builder/' . get_row_layout() . '.php', false, false)); ?>
         <?php endwhile; ?>
-
 </div>
