df18092cda909e2609ee42e34d1dbe69e7a2d0cf
Mattia <guzzi.mattia@gmail.com>
2017-01-17 11:16:45 +0100

    no message

--- builder/riga.php
+++ builder/riga.php
@@ -1,15 +1,12 @@
+
 <div class="grid-12">
-    <?php while (have_rows('riga')) : the_row(); ?>
+    <?php while(have_rows('riga')) : the_row(); ?>
         <div class="grid-2">
-            <?php var_dump(get_row_layout() == 'colonna') ?>
-            <?php if (get_row_layout() == 'colonna'): ?>
-
-               <!-- <?php /*while (have_rows('colonna')) : the_row(); */?>
-                    <?php /*echo get_sub_field('n_cols'); */?>
-                    <?php /*include(locate_template(get_row_layout() . '.php', false, false)); */?>
-                --><?php /*endwhile; */?>
-
-            <?php endif ?>
+        <?php while(have_rows('cols')) : the_row(); ?>
+            <div class="prova">
+            <?php include(locate_template( get_row_layout().'.php', false, false)); ?>
+            </div>
+        <?php endwhile; ?>
         </div>
     <?php endwhile; ?>
 </div>
