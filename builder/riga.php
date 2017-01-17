32e4423fbf9b8d4d0ebe78be46395f2aa1937094
Mattia <guzzi.mattia@gmail.com>
2017-01-17 11:30:28 +0100

    no message

--- builder/riga.php
+++ builder/riga.php
@@ -1,14 +1,9 @@
 
 <div class="grid-12">
-    <?php while(have_rows('riga')) : the_row(); ?>
-        <div class="grid-2">
-            <?php the_sub_field('n_cols'); ?>
+    <?php var_dump(have_rows('riga'))?>
+    <?php var_dump(the_sub_field('n_cols'))?>
 
-        <?php while(have_rows('colonna')) : the_row(); ?>
-            <div class="prova">
-            <?php include(locate_template( get_row_layout().'.php', false, false)); ?>
-            </div>
-        <?php endwhile; ?>
-        </div>
-    <?php endwhile; ?>
+
+
+
 </div>
