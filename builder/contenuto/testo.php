9eee988b01bd303d404674e02c63cd2fe17a85c4
Mattia <guzzi.mattia@gmail.com>
2017-01-17 16:01:02 +0100

    no message

--- builder/contenuto/testo.php
+++ builder/contenuto/testo.php
@@ -1,2 +1,3 @@
-<h3 class="title"><?php if(get_sub_field('titolo') != '') : echo get_sub_field('titolo'); endif ?></h3>
+<?php while ( have_rows('testo') ) : the_row(); ?>
+<h3 class="title"><?php echo get_sub_field('titolo') ?></h3>
 <div class="description"><?php echo get_sub_field('contenuto') ?></div>
