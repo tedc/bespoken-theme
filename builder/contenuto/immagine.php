2f94a564fa739b99e5041861976018726e8725cb
Mattia <guzzi.mattia@gmail.com>
2017-01-18 10:46:12 +0100

    no message

--- builder/contenuto/immagine.php
+++ builder/contenuto/immagine.php
@@ -1,5 +1,6 @@
 <figure class="image">
     <?php if( !get_field('immagine') ): ?>
+        <?php var_dump(the_field('immagine')); ?>
     <img class="" src="<?php the_field('immagine'); ?>">
     <?php endif ?>
 </figure>
