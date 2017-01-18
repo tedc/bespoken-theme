79691e30d0798005a973b34ad8ee16153ad15cce
Mattia <guzzi.mattia@gmail.com>
2017-01-18 17:40:40 +0100

    no message

--- builder/colonna.php
+++ builder/colonna.php
@@ -13,9 +13,22 @@
         $col = '8';
         break;
 } ?>
+<?php $bg_src = get_sub_field('bg_img')['url'] ?>
 
-<div class="col-<?php echo $col?> <?php echo get_sub_field('colore_sfondo')?> <?php echo get_sub_field('posizione_verticale')?> <?php echo get_sub_field('no_padding')? 'no-padding' : ''?>">
-    <?php while(have_rows('contenuto')) : the_row(); ?>
+<?php if (get_sub_field('bg_kind') == 'cover') :
+    $bg_kind = 'background-size: cover;';
+elseif (get_sub_field('bg_kind') == 'contain'):
+    $bg_kind = 'background-size: contain;';
+elseif (get_sub_field('bg_kind') == 'repeat'):
+    $bg_kind = 'background-size: contain; background-repeat: repeat;';
+else :
+$bg_kind = '';
+endif;
+?>
+
+<div
+    class="col-<?php echo $col ?> <?php echo get_sub_field('colore_sfondo') ?> <?php echo get_sub_field('posizione_verticale') ?> <?php echo get_sub_field('no_padding') ? 'no-padding' : '' ?>" <?php if (!empty(get_sub_field('bg_img'))): 'style="background-image:url(' . $bg_src . ');' . $bg_kind; endif ?>>
+    <?php while (have_rows('contenuto')) : the_row(); ?>
         <?php include(locate_template('builder/contenuto/' . get_row_layout() . '.php', false, false)); ?>
     <?php endwhile; ?>
 </div>
