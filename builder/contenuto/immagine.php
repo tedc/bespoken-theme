235b40429b634278ce461bcb86d247048bbd07e9
Mattia <guzzi.mattia@gmail.com>
2017-01-18 10:57:47 +0100

    no message

--- builder/contenuto/immagine.php
+++ builder/contenuto/immagine.php
@@ -1,3 +1,3 @@
-<figure class="image">
-    <img class="" src="<?php the_sub_field('immagine'); ?>">
+<figure class="figure <?php echo get_sub_field('full')? 'full' : ''?>">
+    <img src="<?php the_sub_field('immagine'); ?>">
 </figure>
