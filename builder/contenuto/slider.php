9115b8add37e6b68e16ade5f0d7f58e7030160e7
Mattia <guzzi.mattia@gmail.com>
2017-01-19 17:30:38 +0100

    no message

--- builder/contenuto/slider.php
+++ builder/contenuto/slider.php
@@ -1,32 +1,22 @@
-<?php if (get_sub_field('cornice') == 'cornice') :
-    $frame = ' frame';
-elseif (get_sub_field('cornice') == 'cornice browser'):
-    $frame = ' browser';
-else :
-    $frame = '';
-endif;
-?>
-
-
 <?php if ((get_sub_field('titolo') != '')) : ?>
     <h3 class="title"><?php echo get_sub_field('titolo') ?></h3>
 <?php endif ?>
 <div class="slider" ng-slider>
-    <ul class="slider-wrapper<?php echo $frame ?><?php echo get_sub_field('full') ? ' full' : '' ?>">
+    <ul class="slider-wrapper<?php get_sub_field('cornice') ? ' frame' : '' ?><?php echo get_sub_field('full') ? ' full' : '' ?>">
         <?php if (get_sub_field('tipologia') == 'immagini') : ?>
             <?php $images = get_sub_field('galleria_immagini'); ?>
             <?php if ($images): ?>
-                <?php foreach ($images as $image): ?>
-                    <li class="slider-item">
+                <?php $n_page = 0; foreach ($images as $image): ?>
+                    <li class="slider-item" ng-class="{current:pos==<?php echo $n_page ?>}">
                         <?php echo wp_get_attachment_image($image["id"], "large") ;?>
                     </li>
-                <?php endforeach; ?>
+                <?php $n_page++; endforeach; ?>
             <?php endif ?>
         <?php elseif (get_sub_field('tipologia') == 'testo') : ?>
             <?php if (have_rows('galleria_testo')): ?>
                 <?php $n_page = 0;
                 while (have_rows('galleria_testo')) : the_row(); ?>
-                    <li class="slider-item">
+                    <li class="slider-item" ng-class="{current:pos==<?php echo $n_page ?>}">
                         <?php the_sub_field('pagine'); ?>
                     </li>
                     <?php $n_page++; endwhile ?>
