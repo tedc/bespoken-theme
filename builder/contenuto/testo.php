41d00d1bdfc5c3c3c44ad5129e0d0af28760320b
Mattia <guzzi.mattia@gmail.com>
2017-01-19 18:00:43 +0100

    no message

--- builder/contenuto/testo.php
+++ builder/contenuto/testo.php
@@ -1,5 +1,9 @@
 <?php while ( have_rows('testo') ) : the_row(); ?>
     <?php $align = ((get_sub_field('allineamento_testo') == 'align-right') ? 'align-right' : (get_sub_field('allineamento_testo') == 'align-center') ? 'align-center' : 'align-left'); ?>
-    <h3 class="title <?php echo $align ?>"><?php echo get_sub_field('titolo') ?></h3>
+    <?php if(!get_sub_field('titolo_precompilato') && trim(get_sub_field('titolo'))!='') :?>
+    <h3 class="title <?php echo $align ?>"><?php the_sub_field('titolo') ?></h3>
+        <?php else : ?>
+        <h2 class="title emphasis"><em><?php the_sub_field('titolo_precompilato');?></em></h2>
+        <?php endif ?>
 <div class="description <?php echo $align ?>"><?php echo get_sub_field('contenuto') ?></div>
 <?php endwhile ?>
