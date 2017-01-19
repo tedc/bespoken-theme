a4c3afdca51452cb1b06dda52d8ce4c29970a80b
Mattia <guzzi.mattia@gmail.com>
2017-01-19 15:46:19 +0100

    no message

--- builder/contenuto/slider.php
+++ builder/contenuto/slider.php
@@ -8,27 +8,36 @@ endif;
 ?>
 
 
-<?php if((get_sub_field('titolo') != '')) : ?>
-<h3 class="title"><?php echo get_sub_field('titolo') ?></h3>
+<?php if ((get_sub_field('titolo') != '')) : ?>
+    <h3 class="title"><?php echo get_sub_field('titolo') ?></h3>
 <?php endif ?>
-<ul class="slider<?php echo $frame ?><?php echo get_sub_field('full') ? ' full' : '' ?>">
-    <?php if (get_sub_field('tipologia') == 'immagini') : ?>
-        <?php $images = get_sub_field('galleria_immagini'); ?>
-        <?php if ($images): ?>
-            <?php foreach ($images as $image): ?>
-                <li class="slider-item slider-item-img">
-                    <img src="<?php echo $image['sizes']['thumbnail']; ?>" alt="<?php echo $image['alt']; ?>"/>
-                </li>
-            <?php endforeach; ?>
+<div class="slider">
+    <ul class="slider-wrapper<?php echo $frame ?><?php echo get_sub_field('full') ? ' full' : '' ?>">
+        <?php if (get_sub_field('tipologia') == 'immagini') : ?>
+            <?php $images = get_sub_field('galleria_immagini'); ?>
+            <?php if ($images): ?>
+                <?php foreach ($images as $image): ?>
+                    <li class="slider-item slider-item-img">
+                        <img src="<?php echo $image['sizes']['thumbnail']; ?>" alt="<?php echo $image['alt']; ?>"/>
+                    </li>
+                <?php endforeach; ?>
+            <?php endif ?>
+        <?php elseif (get_sub_field('tipologia') == 'testo') : ?>
+            <?php if (have_rows('galleria_testo')): ?>
+                <?php while (have_rows('galleria_testo')) : the_row(); ?>
+                    <li class="slider-item slider-item-txt">
+                        <span class="page"><?php the_sub_field('pagine'); ?></span>
+                    </li>
+                <?php endwhile ?>
+            <?php endif ?>
         <?php endif ?>
-    <?php elseif (get_sub_field('tipologia') == 'testo') : ?>
-        <?php if (have_rows('galleria_testo')): ?>
-            <?php while (have_rows('galleria_testo')) : the_row(); ?>
-                <li class="slider-item slider-item-txt">
-                    <span class="page"><?php the_sub_field('pagine'); ?></span>
-                </li>
-            <?php endwhile ?>
-        <?php endif ?>
+        <div class="block-transition"></div>
+    </ul>
+    <?php if (get_sub_field('navigatore') == 'number') : ?>
+        <nav class="nav-number">
+
+        </nav>
+    <?php elseif (get_sub_field('navigatore') == 'arrow') : ?>
+        <nav class="nav-arrow"></nav>
     <?php endif ?>
-    <div class="block-transition"></div>
-</ul>
+</div>
