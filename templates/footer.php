8bcb80be55671c9bf09e54d00840e494e88587c5
Mattia <guzzi.mattia@gmail.com>
2017-01-19 16:48:28 +0100

    no message

--- templates/footer.php
+++ templates/footer.php
@@ -1,18 +1,14 @@
-<footer class="content-info">
-    <section class="intestazione">
-        <span class="intestazione-text"><?php the_field('intestazione', 'option'); ?></span>
-        <a class="link-cookie"
-           href="<?php the_field('link_page_cookie', 'option'); ?>"><?php _e('Cookie e Privacy Policy', 'bspkn') ?></a>
-    </section>
-    <section class="sub-footer bg-gradient">
-        <span class="sub-footer-text"><?php the_field('frase_sub_footer', 'option'); ?></span>
-        <a href="<?php the_field('contact_page', 'option'); ?>">
-            <span class="btn">
+<footer class="footer">
+    <?php the_field('intestazione', 'option'); ?>
+    <a class="link-cookie"
+       href="<?php the_field('link_page_cookie', 'option'); ?>"><?php _e('Cookie e Privacy Policy', 'bspkn') ?></a>
+</footer>
+<nav class="cat">
+    <span class="cat-text"><?php the_field('frase_sub_footer', 'option'); ?></span>
+    <a class="btn" href="<?php the_field('contact_page', 'option'); ?>">
                     <span class="btn-line">
                         <span class="btn-arrow-up"></span>
                         <span class="btn-arrow-down"></span>
                     </span>
-            </span>
-        </a>
-    </section>
-</footer>
+    </a>
+</nav>
