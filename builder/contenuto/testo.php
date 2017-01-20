928b6733ddbec3af6878f6e14b1d127eda8d9b5a
Mattia <guzzi.mattia@gmail.com>
2017-01-20 10:23:41 +0100

    no message

--- builder/contenuto/testo.php
+++ builder/contenuto/testo.php
@@ -1,8 +1,7 @@
 <?php while ( have_rows('testo') ) : the_row(); ?>
     <?php $align = ((get_sub_field('allineamento_testo') == 'align-right') ? ' align-right' : (get_sub_field('allineamento_testo') == 'align-center') ? ' align-center' : ' align-left'); ?>
     <?php if(!get_sub_field('titolo_precompilato') && trim(get_sub_field('titolo'))!='') :?>
-        <?php var_dump(get_field('enfasi_titolo'))?>
-    <h3 class="title<?php echo $align ?><?php echo (get_field('enfasi_titolo')? ' emphasis' : '' )  ?>"><?php the_sub_field('titolo') ?> </h3>
+    <h3 class="title<?php echo $align ?><?php echo (get_sub_field('enfasi_titolo')? ' emphasis' : '' )  ?>"><?php the_sub_field('titolo') ?> </h3>
         <?php else : ?>
         <h2 class="title emphasis"><em><?php the_sub_field('titolo_precompilato');?></em></h2>
         <?php endif ?>
