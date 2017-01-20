d163530a99c7c30bfe62ddc5eb6d75bc38a5246d
Mattia <guzzi.mattia@gmail.com>
2017-01-20 17:05:51 +0100

    no message

--- builder/colonna.php
+++ builder/colonna.php
@@ -23,11 +23,28 @@ elseif (get_sub_field('bg_kind') == 'contain'):
 elseif (get_sub_field('bg_kind') == 'repeat'):
     $bg_kind = 'background-size: contain; background-repeat: repeat;';
 else :
-$bg_kind = '';
+    $bg_kind = '';
 endif;
 ?>
 
-<div class="col-<?php echo $col ?> <?php echo get_sub_field('colore_sfondo') ?> <?php echo get_sub_field('posizione_verticale') ?> <?php echo get_sub_field('no_padding') ? 'no-padding' : '' ?>"<?php echo (get_sub_field('bg_img') != '') ? ' style="background-image:url(' . $bg_src . ');' . $bg_kind . '"' : '' ; ?>>
+<?php if (get_sub_field('spaziatura') == '') :
+    $padding = 'row-lg';
+elseif (get_sub_field('grandezza_spaziatura') == 'row'):
+    $padding = 'row';
+elseif (get_sub_field('grandezza_spaziatura') == 'row-lg'):
+    $padding = 'row-lg';
+elseif(get_sub_field('grandezza_spaziatura_sopra') == 'row'):
+    $padding = 'row-lg row-top';
+elseif(get_sub_field('grandezza_spaziatura_sopra') == 'row-lg'):
+$padding = 'row-lg';
+elseif(get_sub_field('grandezza_spaziatura_sotto') == 'row'):
+$padding = 'row-lg row-btn';
+elseif(get_sub_field('grandezza_spaziatura_sotto') == 'row-lg'):
+$padding = 'row-lg';
+?>
+
+<div
+    class="col-<?php echo $col ?> <?php echo $padding ?> <?php echo get_sub_field('colore_sfondo') ?> <?php echo get_sub_field('posizione_verticale') ?> <?php echo get_sub_field('no_padding') ? 'no-padding' : '' ?>"<?php echo (get_sub_field('bg_img') != '') ? ' style="background-image:url(' . $bg_src . ');' . $bg_kind . '"' : ''; ?>>
     <?php while (have_rows('contenuto')) : the_row(); ?>
         <?php include(locate_template('builder/contenuto/' . get_row_layout() . '.php', false, false)); ?>
     <?php endwhile; ?>
