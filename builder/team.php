498b8cb1ccd36ac36c43d02b64398fffcb1fea0a
Mattia <guzzi.mattia@gmail.com>
2017-01-21 17:13:13 +0100

    no message

--- builder/team.php
+++ builder/team.php
@@ -8,12 +8,22 @@
                 ?>
             </figure>
             <div class="user-details">
-                <div class="user-details-name"><?php echo $user["user_firstname"].' '.$user["user_lastname"] ?></div>
+                <div
+                    class="user-details-name"><?php echo $user["user_firstname"] . ' ' . $user["user_lastname"] ?></div>
                 <nav class="user-details-social">
-                    <a class="icon-linkedin" href="<?php $user["user_url"]; ?>"></a>
-                    <a class="icon-facebook" href="<?php the_author_meta( 'facebook',  $user["ID"] ); ?>"></a>
-                    <a class="icon-twitter" href="https://twitter.com/<?php the_author_meta( 'twitter',  $user["ID"] ); ?>"></a>
-                    <a class="icon-instagram" href="<?php the_author_meta( 'google_plus',  $user["ID"] ); ?>"></a>
+                    <?php if ($user["user_url"] != '') : ?>
+                        <a class="icon-linkedin" href="<?php echo $user["user_url"]; ?>" target="_blank"></a>
+                    <?php endif ?>
+                    <?php if (get_the_author_meta('facebook') != '') : ?>
+                        <a class="icon-facebook" href="<?php the_author_meta('facebook', $user["ID"]); ?>" target="_blank"></a>
+                    <?php endif ?>
+                    <?php if (get_the_author_meta('twitter') != '') : ?>
+                        <a class="icon-twitter"
+                           href="https://twitter.com/<?php the_author_meta('twitter', $user["ID"]); ?>" target="_blank"></a>
+                    <?php endif ?>
+                    <?php if (get_the_author_meta('google_plus') != '') : ?>
+                        <a class="icon-instagram" href="<?php the_author_meta('google_plus', $user["ID"]); ?>" target="_blank"></a>
+                    <?php endif ?>
                 </nav>
             </div>
         </div>
