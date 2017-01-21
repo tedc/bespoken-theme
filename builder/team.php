8e49a80820321ed09df7bc598a6bfe3904657937
Mattia <guzzi.mattia@gmail.com>
2017-01-21 15:20:51 +0100

    no message

--- builder/team.php
+++ builder/team.php
@@ -2,9 +2,11 @@
 <?php if( $users ): ?>
 <?php foreach( $users as $user ): ?>
 <div class="col-<?php the_sub_field("n_cols") ?> no-padding">
+    <figure class="user-image">
     <?php
     echo get_avatar( 'guzzi.mattia@gmail.com', $size = '500' );
     ?>
+    </figure>
 </div>
 <?php endforeach; ?>
 <?php endif; ?>
