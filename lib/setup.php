def2abfbc3766ce2e37fcf7602598ec89a279a9b
tedc <egrandinetti@gmail.com>
2017-01-21 20:19:47 +0100

    scripts

--- lib/setup.php
+++ lib/setup.php
@@ -100,7 +100,7 @@ function assets() {
   if (is_single() && comments_open() && get_option('thread_comments')) {
     wp_enqueue_script('comment-reply');
   }
-
-  wp_enqueue_script('sage/js', Assets\asset_path('scripts/main.js'), ['jquery'], null, true);
+  wp_enqueue_script( 'lib', '', Assets\asset_path('scripts/lib.js'), null, null, true );
+  wp_enqueue_script('sage/js', Assets\asset_path('scripts/main.js'), ['lib'], null, true);
 }
 add_action('wp_enqueue_scripts', __NAMESPACE__ . '\\assets', 100);
