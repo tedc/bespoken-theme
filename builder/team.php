2b4aed5452ac152bf1187ffd23359b1301a445f9
Mattia <guzzi.mattia@gmail.com>
2017-01-21 15:12:19 +0100

    no message

--- builder/team.php
+++ builder/team.php
@@ -2,7 +2,9 @@
 <?php if( $users ): ?>
 <?php foreach( $users as $user ): ?>
 <div class="col-<?php the_sub_field("n_cols") ?>">
-    <?php echo $user["user_avatar"]?>
+    <?php
+    echo get_avatar( $user->user_email, $size = '500' );
+    ?>
 </div>
 <?php endforeach; ?>
 <?php endif; ?>
