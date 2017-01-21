0264294ca76e045e7a351608ae27823433c01557
Mattia <guzzi.mattia@gmail.com>
2017-01-21 15:36:17 +0100

    no message

--- builder/team.php
+++ builder/team.php
@@ -1,13 +1,16 @@
 <?php $users = get_sub_field('utenti') ?>
-<?php if( $users ): ?>
-<?php foreach( $users as $user ): ?>
-        <?php var_dump($user) ?>
-<div class="col-<?php the_sub_field("n_cols") ?> no-padding">
-    <figure class="user-image">
-    <?php
-    echo get_avatar( $user["user_email"], $size = '500' );
-    ?>
-    </figure>
-</div>
-<?php endforeach; ?>
+<?php if ($users): ?>
+    <?php foreach ($users as $user): ?>
+        <div class="col-<?php the_sub_field("n_cols") ?> no-padding">
+            <figure class="user-image">
+                <?php
+                echo get_avatar($user["user_email"], $size = '500');
+                ?>
+            </figure>
+            <div class="user-details">
+                <div class="user-details-name"><?php echo $user["user_firstname"].' '.$user["user_lastname"] ?></div>
+                <div class="user-details-social"></div>
+            </div>
+        </div>
+    <?php endforeach; ?>
 <?php endif; ?>
