0c3b891b4e099369d4b5b40a58d677db52e6e270
tedc <egrandinetti@gmail.com>
2017-01-21 11:28:53 +0100

    header

--- templates/header.php
+++ templates/header.php
@@ -1,7 +1,7 @@
 <a class="logo icon-logo" href="<?= esc_url(home_url('/')); ?>"></a>
 <a class="btn-menu" href="#" ng-click="openMenu">
-    <span class="btn-menu-text" ng-menu-text></span>
-	<span class="toggle" data-close="<?php _e('Chiudi', 'bspkn'); ?>">
+    <span class="btn-menu-text" ng-menu-text><?php _e('Menu', 'bspkn'); ?></span>
+	<span class="toggle">
 		<span class="toggle-line"></span>
 		<span class="toggle-line"></span>
 		<span class="toggle-line"></span>
