4a641a87aeb6503011916cafc89283a1f97d6459
Mattia <guzzi.mattia@gmail.com>
2017-01-21 11:02:56 +0100

    no message

--- builder/contenuto/testo.php
+++ builder/contenuto/testo.php
@@ -9,9 +9,6 @@ elseif (get_sub_field('spaziatura') == '1'):
 elseif (get_sub_field('spaziatura') == '2'):
     $padding = get_sub_field('grandezza_spaziatura_sotto');
 endif;?>
-    <?php the_sub_field('spaziatura') ?>
-    <?php the_sub_field('grandezza_spaziatura'); ?>
- <?php echo $padding ?>
 
 <div class="content <?php echo $padding ?>">
     <?php $align = ((get_sub_field('allineamento_testo'))); ?>
