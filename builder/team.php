626c5faba426d02aa4be55536b635d0fa0bedc6d
Mattia <guzzi.mattia@gmail.com>
2017-01-21 14:40:51 +0100

    no message

--- builder/team.php
+++ builder/team.php
@@ -1,4 +1,8 @@
+<?php $users = get_sub_field('utenti') ?>
+<?php if( $users ): ?>
+<?php foreach( $users as $user ): ?>
 <div class="col-<?php the_sub_field("n_cols") ?>">
-    <?php var_dump(get_sub_field('utenti')) ?>
-   
+    <?php var_dump($user) ?>
 </div>
+<?php endforeach; ?>
+<?php endif; ?>
