60837dbec7ab6ed0cd6246165a477acd84ed9eed
tedc <egrandinetti@gmail.com>
2017-01-21 20:26:04 +0100

    carousel

--- front-page.php
+++ front-page.php
@@ -1,14 +1,16 @@
-<div class="grid-12" ng-carousel ng-mouse-wheel-up="move(false)" ng-mouse-wheel-down="move(true)">
-	<div class="col-4 carousel-item">
-	</div>
-	<div class="col-4 carousel-item">
-	</div>
-	<div class="col-4 carousel-item">
-	</div>
-	<div class="col-4 carousel-item">
-	</div>
-	<div class="col-4 carousel-item">
-	</div>
-	<div class="col-4 carousel-item">
+<div  ng-carousel ng-mouse-wheel-up="move(false)" ng-mouse-wheel-down="move(true)">
+	<div class="grid-12">
+		<div class="col-4 carousel-item">
+		</div>
+		<div class="col-4 carousel-item">
+		</div>
+		<div class="col-4 carousel-item">
+		</div>
+		<div class="col-4 carousel-item">
+		</div>
+		<div class="col-4 carousel-item">
+		</div>
+		<div class="col-4 carousel-item">
+		</div>
 	</div>
 </div>
