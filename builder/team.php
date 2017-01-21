a2723b08e3b0cb1e81f5abc04e853246963788f3
Mattia <guzzi.mattia@gmail.com>
2017-01-21 17:44:48 +0100

    no message

--- builder/team.php
+++ builder/team.php
@@ -11,20 +11,20 @@
                 <div
                     class="user-details-name"><?php echo $user["user_firstname"] . ' ' . $user["user_lastname"] ?></div>
                 <nav class="user-details-social">
-                    <?php $user_info = get_user_meta($user["ID"]);
-                    var_dump($user_info); ?>
+                   <!-- --><?php /*$user_info = get_user_meta($user["ID"]);
+                    var_dump($user_info); */?>
                     <?php if ($user["user_url"] != '') : ?>
                         <a class="icon-linkedin" href="<?php echo $user["user_url"]; ?>" target="_blank"></a>
                     <?php endif ?>
                     <?php var_dump( get_user_meta('facebook', $user["ID"]))?>
-                        <a class="icon-facebook" href="<?php echo get_user_meta('facebook', $user["ID"]); ?>" target="_blank"></a>
+                        <a class="icon-facebook" href="<?php echo $user_info['facebook']; ?>" target="_blank"></a>
 
 
                         <a class="icon-twitter"
                            href="https://twitter.com/<?php echo get_user_meta('twitter', $user["ID"]); ?>" target="_blank"></a>
 
                     <?php var_dump( get_user_meta('googleplus', $user["ID"]))?>
-                        <a class="icon-instagram" href="<?php echo get_user_meta('googleplus', $user["ID"]); ?>" target="_blank"></a>
+                        <a class="icon-instagram" href="<?php echo $user_info['googleplus']; ?>" target="_blank"></a>
 
                 </nav>
             </div>
