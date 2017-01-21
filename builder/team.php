d165d1622d4da0d94bd9872615086118015846b2
Mattia <guzzi.mattia@gmail.com>
2017-01-21 15:55:03 +0100

    no message

--- builder/team.php
+++ builder/team.php
@@ -1,7 +1,7 @@
 <?php $users = get_sub_field('utenti') ?>
 <?php if ($users): ?>
     <?php foreach ($users as $user): ?>
-        <div class="col-<?php the_sub_field("n_cols") ?> no-padding">
+        <div class="col-<?php the_sub_field("n_cols") ?> user-col">
             <figure class="user-image">
                 <?php
                 echo get_avatar($user["user_email"], $size = '500');
