152ce99342056c37bf8b23501249912c91b52980
Mattia <guzzi.mattia@gmail.com>
2017-01-21 17:09:04 +0100

    no message

--- builder/team.php
+++ builder/team.php
@@ -8,13 +8,12 @@
                 ?>
             </figure>
             <div class="user-details">
-                <?php var_dump($user); ?>
                 <div class="user-details-name"><?php echo $user["user_firstname"].' '.$user["user_lastname"] ?></div>
                 <nav class="user-details-social">
                     <a class="icon-linkedin" href="<?php $user["user_url"]; ?>"></a>
                     <a class="icon-facebook" href="<?php the_author_meta( 'facebook',  $user["ID"] ); ?>"></a>
-                    <a class="icon-twitter" href="<?php the_author_meta( 'twitter',  $user["ID"] ); ?>"></a>
-                    <a class="icon-instagram" href="<?php the_author_meta( 'google',  $user["ID"] ); ?>"></a>
+                    <a class="icon-twitter" href="https://twitter.com/<?php the_author_meta( 'twitter',  $user["ID"] ); ?>"></a>
+                    <a class="icon-instagram" href="<?php the_author_meta( 'google_plus',  $user["ID"] ); ?>"></a>
                 </nav>
             </div>
         </div>
