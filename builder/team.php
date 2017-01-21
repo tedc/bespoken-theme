81b4231b42f0dfab9fedb507dad5b00ddd38162e
Mattia <guzzi.mattia@gmail.com>
2017-01-21 12:17:30 +0100

    no message

--- builder/team.php
+++ builder/team.php
@@ -1,4 +1,6 @@
 <?php if (have_rows('team')) : ?>
     <?php while (have_rows('team')) : the_row(); ?>
+        <div class="col-<?php get_sub_field("n_cols")?>">
+        </div>
     <?php endwhile; ?>
 <?php endif; ?>
