d521f5401385c7149633e56df0f720a77a3df51c
tedc <egrandinetti@gmail.com>
2017-01-22 11:53:52 +0100

    push

---
+++ extras/utils.php
@@ -0,0 +1,4 @@
+<?php
+	function close($ngClick) {
+		echo '<span class="btn close" ng-clic="'.$ngClick.'"><span class="btn-line"></span><span class="btn-line"></span></span>';
+	}
--- templates/modal-contact.php
+++ templates/modal-contact.php
@@ -1,3 +1,28 @@
 <div class="modal-container" ng-class="{visible : isContact}">
-	
+	<?php close('isModal=false;isContact=false'); ?>
+	<form class="grid-12 form">
+		<div class="col-4 alternate form-col">
+		</div>
+		<div class="col-8 dark form-col">
+			<div class="grid-12 row-lg">
+				<div class="col-6">
+					<div class="row-input">
+						<input type="text" name="sender" placeholder="<?php _e('Nome e cognome', 'bspkn'); ?>">
+					</div>
+					<div class="row-input">
+						<input type="email" name="email" required placeholder="<?php _e('Indirizzo email (richiesto)', 'bspkn'); ?>">
+					</div>
+					
+					<input type="tel" name="tel" required placeholder="<?php _e('Telefono (richiesto)', 'bspkn'); ?>">
+				</div>
+				<div class="col-6">
+					<div class="row-input">
+					<textarea name="message" placeholder="<?php _e('Messaggio', 'bspkn'); ?>"></textarea>
+					<button class="btn-send">
+						<?php _e('Invia', 'bspkn'); ?>
+					</button>
+				</div>
+			</div>
+		</div>
+	</form>
 </div>
