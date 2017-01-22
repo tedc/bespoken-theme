73169345a8847b3879f00828a062173cc0b213f6
tedc <egrandinetti@gmail.com>
2017-01-22 19:47:07 +0100

    front page

--- front-page.php
+++ front-page.php
@@ -1,4 +1,4 @@
-<div ng-carousel ng-mouse-wheel-up="scroll(false, 5)" ng-mouse-wheel-down="scroll(true, 5)" items="3">
+<div ng-carousel ng-mouse-wheel-up="scroll(true, 5)" ng-mouse-wheel-down="scroll(false, 5)" items="3">
 	<div class="carousel-wrapper">
 	<div class="carousel-item">
 		<a href="#" class="btn"><span class="btn-text"><?php _e('Scopri', 'bspkn'); ?></span></a>
@@ -15,7 +15,7 @@
 	</div>
 	</div>
 	<nav class="carousel-nav">
-		<span ng-click="move(false, 5)">prima</span>
-		<span ng-click="move(true, 5)">dopo</span>
+		<span ng-click="move(true, 5)">prima</span>
+		<span ng-click="move(false, 5)">dopo</span>
 	</nav>
 </div>
