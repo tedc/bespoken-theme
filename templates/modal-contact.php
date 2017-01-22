7b520821e14ddeba1ece187da70b32c1ea14329c
tedc <egrandinetti@gmail.com>
2017-01-22 15:19:56 +0100

    form

--- templates/modal-contact.php
+++ templates/modal-contact.php
@@ -1,28 +1,32 @@
 <div class="modal-container" ng-class="{visible : isContact}">
 	<?php close('isModal=false;isContact=false'); ?>
-	<form class="grid-12 form">
-		<div class="col-4 alternate form-col">
+	<form class="form">
+		<div class="form-content">
 		</div>
-		<div class="col-8 dark form-col">
-			<div class="grid-12 row-lg">
-				<div class="col-6">
-					<div class="row-input">
-						<input type="text" name="sender" placeholder="<?php _e('Nome e cognome', 'bspkn'); ?>">
-					</div>
-					<div class="row-input">
-						<input type="email" name="email" required placeholder="<?php _e('Indirizzo email (richiesto)', 'bspkn'); ?>">
-					</div>
-					
-					<input type="tel" name="tel" required placeholder="<?php _e('Telefono (richiesto)', 'bspkn'); ?>">
+		<div class="form-inputs">
+			<div class="form-col">
+				<div class="form-row">
+					<input type="text" name="sender" placeholder="<?php _e('Nome e cognome', 'bspkn'); ?>">
 				</div>
-				<div class="col-6">
-					<div class="row-input">
-					<textarea name="message" placeholder="<?php _e('Messaggio', 'bspkn'); ?>"></textarea>
-					<button class="btn-send">
-						<?php _e('Invia', 'bspkn'); ?>
-					</button>
+				<div class="form-row">
+					<input type="email" name="email" required placeholder="<?php _e('Indirizzo email (richiesto)', 'bspkn'); ?>">
 				</div>
+				
+				<input type="tel" name="tel" required placeholder="<?php _e('Telefono (richiesto)', 'bspkn'); ?>">
 			</div>
+			<div class="form-col">
+				<div class="orm-row">
+					<textarea name="message" placeholder="<?php _e('Messaggio', 'bspkn'); ?>"></textarea>
+				</div>
+				<button class="btn-send">
+					<?php _e('Invia', 'bspkn'); ?>
+					<span class="btn">
+						<span class="btn-line">
+							<span class="btn-arrow-up"></span>
+							<span class="btn-arrow-up"></span>
+						</span>
+					</span>
+				</button>
 		</div>
 	</form>
 </div>
