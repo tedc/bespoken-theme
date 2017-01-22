05a780ae63cc26c400868d975ecb7fdb5ac2d1cb
Mattia <guzzi.mattia@gmail.com>
2017-01-22 19:46:28 +0100

    no message

--- builder/contenuto/slider.php
+++ builder/contenuto/slider.php
@@ -22,7 +22,7 @@ endif;?>
                     <?php $n_page = 0;
                     foreach ($images as $image): ?>
                         <li class="slider-item" ng-class="{current:pos==<?php echo $n_page ?>}">
-                            <?php echo wp_get_attachment_image($image["id"], "large"); ?>
+                            <figure><?php echo wp_get_attachment_image($image["id"], "large"); ?></figure>
                         </li>
                         <?php $n_page++; endforeach; ?>
                 <?php endif ?>
