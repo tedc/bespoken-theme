84dfe54197a091a500e65aa7c829302370f93fd1
tedc <egrandinetti@gmail.com>
2017-01-22 17:40:33 +0100

    cursor speed

--- templates/modal-contact.php
+++ templates/modal-contact.php
@@ -1,23 +1,28 @@
 <div class="modal-container" ng-class="{visible : isContact}">
 	<?php close('isModal=false;isContact=false'); ?>
-	<form class="form">
+	<form class="form" name="contactForm" ng-submit="onSubmit(contactForm.$valid)">
 		<div class="form-content row-lg">
+			<div class="content">
+				<?php the_field('testo_contatti', 'options'); ?>
+				<a href="https://www.facebook.com/bspkn.studio/" class="icon-facebook"></a>
+				<a href="https://www.linkedin.com/company/10485978" class="icon-linkedin"></a>
+			</div>
 		</div>
 		<div class="form-inputs row-lg">
 			<div class="form-col">
 				<div class="form-row">
-					<input type="text" name="sender" placeholder="<?php _e('Nome e cognome', 'bspkn'); ?>">
+					<input type="text" name="sender" placeholder="<?php _e('Nome e cognome', 'bspkn'); ?>" ng-model="sender">
 				</div>
 				<div class="form-row">
-					<input type="email" name="email" required placeholder="<?php _e('Indirizzo email (richiesto)', 'bspkn'); ?>">
+					<input type="email" name="email" required placeholder="<?php _e('Indirizzo email (richiesto)', 'bspkn'); ?>" ng-model="email">
 				</div>			
-				<input type="tel" name="tel" required placeholder="<?php _e('Telefono (richiesto)', 'bspkn'); ?>">
+				<input type="tel" name="tel" required placeholder="<?php _e('Telefono (richiesto)', 'bspkn'); ?>" ng-model="tel">
 			</div>
 			<div class="form-col">
 				<div class="form-row">
-					<textarea name="message" placeholder="<?php _e('Messaggio', 'bspkn'); ?>"></textarea>
+					<textarea name="message" placeholder="<?php _e('Messaggio', 'bspkn'); ?>" ng-model="message"></textarea>
 				</div>
-				<button class="btn-send">
+				<button class="btn-send" ng-disabled="contactForm.$invalid && !isPrivacyCheckd">
 					<?php _e('Invia', 'bspkn'); ?>
 					<span class="btn">
 						<span class="btn-line">
