55f944daed1108cbc43d1da0ba8d7f8921e7126c
Mattia <guzzi.mattia@gmail.com>
2017-01-23 17:19:26 +0100

    no message

--- builder/intervista.php
+++ builder/intervista.php
@@ -1,5 +1,5 @@
 <div class="video contain">
-    <?php $file = preg-replace('/\\.[^.\\s]{3,4}$/', '', get-sub-field('file')); ?>
+    <?php $file = preg_replace('/\\.[^.\\s]{3,4}$/', '', get_sub_field('file')); ?>
     <div class="container-flow" style="background-image: url(<?php echo $file ?>.jpg)">
         <video poster="<?php echo $file ?>.jpg" class="flowplayer" id="player" loop muted>
             <source src="<?php echo $file ?>.mp4" type="video/mp4">
@@ -7,17 +7,17 @@
             <source src="<?php echo $file ?>.ogv" type="video/ogv">
         </video>
         <div class="video-foreground">
-            <?php $oembed = get-sub-field('video-embed');
+            <?php $oembed = get_sub_field('video_embed');
             echo $oembed;?>
         </div>
     </div>
-    <h3 class="title-video"><?php the-sub-field('titolo') ?></h3>
-    <?php if (get-sub-field('video-embed') != ""){?>
+    <h3 class="title-video"><?php the_sub_field('titolo') ?></h3>
+    <?php if (get_sub_field('video_embed') != ""){?>
         <div class="container-play">
             <div id="play-button" class="icon-play play">PLAY</div>
         </div>
         <div class="container-close">
-            <div class="close-button"><?php -e("chiudi", "bspkn")?></div>
+            <div class="close-button"><?php _e("chiudi", "bspkn")?></div>
         </div>
     <?php } ?>
 </div>
