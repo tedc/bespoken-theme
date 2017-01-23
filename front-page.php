3c850aa06dea4618c62ad2b8e44767fd917f8312
tedc <egrandinetti@gmail.com>
2017-01-23 20:25:25 +0100

    home intro

--- front-page.php
+++ front-page.php
@@ -2,27 +2,18 @@
 	<div class="gradient"></div>
 	<h2 class="home-title">
 		<span class="home-title-row">
-			<span class="home-title-word">
-				<?php 
-				foreach (explode(' ', strip_tags(get_field('first_row'), '<a>')) as $word) {
-					echo $word .'</span> <span class="home-title-word">';
-				} ?>
+			<span class="home-title-sentence">
+				<?php echo strip_tags(get_field('first_row'), '<a>'); ?>
 			</span>
 		</span><br />
 		<span class="home-title-row">
-			<span class="home-title-word">
-				<?php 
-				foreach (explode(' ', strip_tags(get_field('second_row'), '<a>')) as $word) {
-					echo $word .'</span> <span class="home-title-word">';
-				} ?>
+			<span class="home-title-sentence">
+				<?php echo strip_tags(get_field('second_row'), '<a>'); ?>
 			</span>
 		</span><br />
 		<span class="home-title-row">
-			<span class="home-title-word">
-				<?php 
-				foreach (explode(' ', strip_tags(get_field('third_row'), '<a>')) as $word) {
-					echo $word .'</span> <span class="home-title-word">';
-				} ?>
+			<span class="home-title-sentence">
+				<?php echo strip_tags(get_field('third_row'), '<a>'); ?>
 			</span>
 		</span>
 	</h2>
