613e772f114e29397a03a43c60c44db77eb31539
Mattia <guzzi.mattia@gmail.com>
2017-01-23 17:45:27 +0100

    no message

--- builder/contenuto/video.php
+++ builder/contenuto/video.php
@@ -8,28 +8,29 @@ elseif (get_sub_field('spaziatura') == '2'):
     $padding = get_sub_field('grandezza_spaziatura_sotto');
 endif;?>
 
-<div class="video <?php echo get_sub_field('full') ? ' full' : 'content '.$padding ?> ">
+<div class="video <?php echo get_sub_field('full') ? ' full' : 'content '.$padding ?>">
+    <?php if (get_sub_field('cornice')== true) :
+        get_template_part('templates/cornice');
+    endif;?>
     <?php $file = preg_replace('/\\.[^.\\s]{3,4}$/', '', get_sub_field('file')); ?>
-    <div class="container_flow" style="background-image: url(<?php echo $file ?>.jpg)">
-        <video poster="<?php echo $file ?>.jpg" class="flowplayer" id="player" loop muted>
+    <div class="container-player" style="background-image: url(<?php echo $file ?>.jpg)">
+        <video poster="<?php echo $file ?>.jpg" class="player" autoplay loop muted>
             <source src="<?php echo $file ?>.mp4" type="video/mp4">
             <source src="<?php echo $file ?>.webm" type="video/webm">
             <source src="<?php echo $file ?>.ogv" type="video/ogv">
         </video>
-
         <div class="video-foreground">
             <?php $oembed = get_sub_field('video_embed');
             echo $oembed;?>
         </div>
-
     </div>
     <h3 class="title-video"><?php the_sub_field('titolo') ?></h3>
     <?php if (get_sub_field('video_embed') != ""){?>
-        <div class="container_play">
-            <div id="play-button" class="icon-play play"></div>
+        <div class="container-play">
+            <div class="icon-play play">PLAY</div>
         </div>
-        <div class="container_close">
-            <div class="close-button"><?php _e("chiudi", "bspkn")?></div>
+        <div class="container-close">
+            <div class="close"><?php _e("chiudi", "bspkn")?></div>
         </div>
     <?php } ?>
 </div>
