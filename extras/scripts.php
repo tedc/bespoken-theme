56f664a87e81f5fb46dcadfcc8702bb51a1ae1e6
tedc <egrandinetti@gmail.com>
2017-01-23 18:59:13 +0100

    webfonts

--- extras/scripts.php
+++ extras/scripts.php
@@ -1,4 +1,5 @@
 <?php
+use Roots\Sage\Assets;
 /**
  * Created by PhpStorm.
  * User: RiMaStInO
@@ -8,6 +9,21 @@
 function vars()
 {
     ?>
+    <script>
+    	var WebFontConfig = {
+		  custom: {
+		    families: ['din'],
+		    urls: ['<?php echo Assets\asset_path('styles/main.css'); ?>']
+		  }
+		};
+		(function(d) {
+			var wf = d.createElement('script'), s = d.scripts[0];
+			wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js';
+			wf.async = true;
+			s.parentNode.insertBefore(wf, s);
+	   	})(document);
+
+    </script>
     <?php
 }
 add_action('wp_footer', 'vars'); ?>
