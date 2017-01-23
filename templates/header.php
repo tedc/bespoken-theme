29cb16304edc324f725cec668e33bcadade32c23
tedc <egrandinetti@gmail.com>
2017-01-23 18:17:43 +0100

    home

--- templates/header.php
+++ templates/header.php
@@ -1,4 +1,10 @@
-<a class="logo icon-logo" href="<?= esc_url(home_url('/')); ?>"></a>
+<?php if(is_front_page()) : ?>
+	<h1 class="logo">
+		<a  href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
+	</h1>
+<?php else : ?>
+	<a class="logo" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
+<?php endif; ?>
 <a class="btn-menu" href="#" ng-click="openMenu">
     <span class="btn-menu-text" ng-menu-text><?php _e('Menu', 'bspkn'); ?></span>
 	<span class="toggle">
