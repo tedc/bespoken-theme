8891b9c472fec078cf0358ad442998bf659c5e9b
Mattia <guzzi.mattia@gmail.com>
2017-01-24 18:06:17 +0100

    no message

--- builder/jobs.php
+++ builder/jobs.php
@@ -1,15 +1,20 @@
 <?php
 $jobs = get_sub_field('posizioni');
-if( $jobs ): ?>
-    <?php foreach( $jobs as $post):?>
+if ($jobs): ?>
+    <?php foreach ($jobs as $post): ?>
         <?php setup_postdata($post); ?>
-    <div class="col-<?php the_sub_field("n_cols") ?> col-jobs">
+        <div class="col-<?php the_sub_field("n_cols") ?> col-jobs" id="job_<?php the_ID(); ?>"
+             ng-class="{visible : isJob == <?php the_ID(); ?>}">
             <figure>
-                <img src="<?php the_field('icon', $post->ID)["url"]?>">
+                <img src="<?php the_field('icon', $post->ID)["url"] ?>">
             </figure>
             <h3 class="job-title"><?php the_title(); ?></h3>
-      <?php the_excerpt(); ?>
-    </div>
+            <?php the_excerpt(); ?>
+            <a href="#" ng-click=”$event.preventDefault(); isJob = <?php the_ID(); ?>" class="permalink"></a>
+            <div class="job-content">
+                <?php the_content() ?>
+            </div>
+        </div>
     <?php endforeach; ?>
-    <?php wp_reset_postdata();?>
+    <?php wp_reset_postdata(); ?>
 <?php endif; ?>
