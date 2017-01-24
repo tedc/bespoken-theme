ffd2982362a37f8eba63d32e897da761f2a728b2
Mattia <guzzi.mattia@gmail.com>
2017-01-24 12:30:50 +0100

    no message

--- templates/header-immagine.php
+++ templates/header-immagine.php
@@ -1,7 +1,11 @@
 <style>
     .cover {
         background-image:url(<?php the_post_thumbnail_url('medium') ?>);
-
+    }
+    @media screen and (min-width: <?php echo 850/16 ?>em) {
+        .cover {
+            background-image: url(<?php the_post_thumbnail_url('large') ?>);
+        }
     }
 </style>
 <div class="cover"></div>
