95f1056762b6454602b20e2d2f47a12f89f1c61a
Mattia <guzzi.mattia@gmail.com>
2017-01-24 13:06:34 +0100

    no message

---
+++ builder/jobs.php
@@ -0,0 +1,14 @@
+<?php
+$jobs = get_field('posizioni');
+if( $jobs ): ?>
+    <?php foreach( $jobs as $job):?>
+        <?php setup_postdata($job); ?>
+    <div class="col-<?php the_sub_field("n_cols") ?>">
+            <figure>
+                <img src="<?php get_sub_field('icon')['url'] ?>">
+            </figure>
+            <?php the_title(); ?>
+    </div>
+    <?php endforeach; ?>
+    <?php wp_reset_postdata();?>
+<?php endif; ?>
