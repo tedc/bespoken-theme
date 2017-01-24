2596a6f35c4c616e60f0d4d8ae1e6a07ba18bbf8
Mattia <guzzi.mattia@gmail.com>
2017-01-24 11:24:04 +0100

    no message

--- templates/header-slideshow.php
+++ templates/header-slideshow.php
@@ -1,1 +1,14 @@
-<div class="slideshow"></div>
+<div class="slider" ng-slider>
+    <ul class="slider-wrapper">
+        <?php $images = get_sub_field('galleria_immagini_slideshow'); ?>
+        <?php if ($images): ?>
+            <?php $n_page = 0;
+            foreach ($images as $image): ?>
+                <li class="slider-item" ng-class="{current:pos==<?php echo $n_page ?>}">
+                    <figure><?php echo wp_get_attachment_image($image["id"], "large"); ?></figure>
+                </li>
+                <?php $n_page++; endforeach; ?>
+            <div class="mask"></div>
+        <?php endif ?>
+    </ul>
+</div>
