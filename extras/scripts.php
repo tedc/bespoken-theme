b2d5c9da45c8bded4e868125da9dbfb419852f3a
tedc <egrandinetti@gmail.com>
2017-01-24 10:49:23 +0100

    remove fonts loader

--- extras/scripts.php
+++ extras/scripts.php
@@ -4,17 +4,17 @@
 	{
 	    ?>
 	    <script>
-	    	var WebFontConfig = {
-			  custom: {
-			    families: ['din']
-			  }
-			};
-			(function(d) {
-				var wf = d.createElement('script'), s = d.scripts[0];
-				wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js';
-				wf.async = true;
-				s.parentNode.insertBefore(wf, s);
-		   	})(document);
+	  //   	var WebFontConfig = {
+			//   custom: {
+			//     families: ['din']
+			//   }
+			// };
+			// (function(d) {
+			// 	var wf = d.createElement('script'), s = d.scripts[0];
+			// 	wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js';
+			// 	wf.async = true;
+			// 	s.parentNode.insertBefore(wf, s);
+		 //   	})(document);
 
 	    </script>
 	    <?php
