ee04ef9852e1adc328a928c40358b6bcddc7a660
Mattia <guzzi.mattia@gmail.com>
2017-01-24 11:00:19 +0100

    no message

--- builder/griglia_immagini.php
+++ builder/griglia_immagini.php
@@ -1,4 +1,5 @@
 <?php $images = get_field('griglia_immagini'); ?>
+<?php var_dump(get_field('griglia_immagini'))?>
 <?php foreach( $images as $image ): ?>
 <div class="col-<?php the_sub_field("n_cols") ?>">
     <figure class="grid-image">
