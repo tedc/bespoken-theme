a537daa014a114e5cda99ff44fe7fa459b972efd
Mattia <guzzi.mattia@gmail.com>
2017-01-24 14:23:26 +0100

    no message

--- builder/jobs.php
+++ builder/jobs.php
@@ -4,12 +4,12 @@ if( $jobs ): ?>
     <?php foreach( $jobs as $post):?>
         <?php setup_postdata($post); ?>
     <div class="col-<?php the_sub_field("n_cols") ?>">
-        <?php var_dump(get_field('icon', $post->ID))?>
-        <?php var_dump(get_field('icon', $post->ID)["url"])?>
+<!--        --><?php /*var_dump(get_field('icon', $post->ID))*/?>
+        <?php var_dump(the_field('icon', $post->ID)["url"])?>
        <!-- <?php /*var_dump(get_sub_field('icon', $post->ID)["url"])*/?>
         --><?php /*get_field('icon', $post->ID)*/?>
             <figure>
-                <img src="<?php get_field('icon', $post->ID)?>">
+                <img src="<?php echo get_field('icon', $post->ID)["url"]?>">
             </figure>
             <?php the_title(); ?>
     </div>
