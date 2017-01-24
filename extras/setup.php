8fb105b442c235b10f52aa1d3fb6d2751f621716
tedc <egrandinetti@gmail.com>
2017-01-24 11:01:11 +0100

    var dump

--- extras/setup.php
+++ extras/setup.php
@@ -11,8 +11,9 @@ if (function_exists('acf_add_options_page')) {
 }
 
 function ng_app($html) {
+	$html =  $html . ' class="no-js" ng-app="bspkn"';
 	var_dump($html);
-	return $html . ' class="no-js" ng-app="bspkn"';
+	return $html;
 }
 
 add_filter( 'language_attributes', 'ng_app' );
