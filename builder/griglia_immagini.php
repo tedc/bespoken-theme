247b7b1ffb016bc693495f931bb391194996593f
Mattia <guzzi.mattia@gmail.com>
2017-01-24 10:58:35 +0100

    no message

--- builder/griglia_immagini.php
+++ builder/griglia_immagini.php
@@ -2,7 +2,7 @@
 <?php foreach( $images as $image ): ?>
 <div class="col-<?php the_sub_field("n_cols") ?>">
     <figure class="grid-image">
-        <img src="<?php echo $image['sizes']['thumbnail']; ?>" alt="<?php echo $image['alt']; ?>" />
+        <img src="<?php echo $image['sizes']['large']; ?>" alt="<?php echo $image['alt']; ?>" />
     </figure>
 </div>
 <?php endforeach; ?>
