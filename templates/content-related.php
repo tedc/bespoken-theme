b623f6f0eb1e278dc4747317737f9d6eae7269da
tedc <egrandinetti@gmail.com>
2017-01-24 15:27:10 +0100

    style

--- templates/content-related.php
+++ templates/content-related.php
@@ -1,10 +1,13 @@
 <a href="<?php the_permalink(); ?>" class="carousel-item">
-	<?php if(get_field('icon_white')) : ?>
-	<figure class="logo">
-		<img src="<?php the_field('icon_white'); ?>" class="logo" />
-	</figure>
-	<?php endif; ?>
-	<h2 class="title"><?php the_title(); ?></h2>
-	<div class="excerpt"><?php the_excerpt(); ?></div>
+	<div class="cover" style="background-image: url(<?php the_post_thumbnail_url('full'); ?>)">
+	<div class="carousel-item-content">
+		<?php if(get_field('icon_white')) : ?>
+		<figure class="logo">
+			<img src="<?php the_field('icon_white'); ?>" class="logo" />
+		</figure>
+		<?php endif; ?>
+		<h2 class="title"><?php the_title(); ?></h2>
+		<div class="excerpt"><?php the_excerpt(); ?></div>
+	</div>
 	<span class="btn"><span class="btn-text"><?php _e('Scopri', 'bspkn'); ?></span>
 </a>
