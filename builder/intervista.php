f244f3d41285734401a924966d2f3406c66e60cd
Mattia <guzzi.mattia@gmail.com>
2017-01-24 18:30:43 +0100

    no message

--- builder/intervista.php
+++ builder/intervista.php
@@ -2,13 +2,7 @@
     <?php if (get_sub_field('cornice') == true) :
         get_template_part('templates/cornice');
     endif; ?>
-    <?php $file = preg_replace('/\\.[^.\\s]{3,4}$/', '', get_sub_field('file')); ?>
-    <div class="container-player" style="background-image: url(<?php echo $file ?>.jpg)">
-        <video poster="<?php echo $file ?>.jpg" class="player" autoplay loop muted>
-            <source src="<?php echo $file ?>.mp4" type="video/mp4">
-            <source src="<?php echo $file ?>.webm" type="video/webm">
-            <source src="<?php echo $file ?>.ogv" type="video/ogv">
-        </video>
+    <div class="container-player" style="background-image: url(<?php echo  get_sub_field('video') ?>)">
         <div class="video-foreground">
             <?php $oembed = get_sub_field('video_embed');
             echo $oembed; ?>
