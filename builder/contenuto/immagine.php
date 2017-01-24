2472816b64c92a11bb519cfc8a9c816adf48eda0
Mattia <guzzi.mattia@gmail.com>
2017-01-24 11:49:37 +0100

    no message

--- builder/contenuto/immagine.php
+++ builder/contenuto/immagine.php
@@ -8,7 +8,7 @@ elseif (get_sub_field('spaziatura') == '2'):
     $padding = get_sub_field('grandezza_spaziatura_sotto');
 endif;?>
 
-<?php $src = the_sub_field('immagine'); ?>
+<?php $src = get_sub_field('immagine'); ?>
 <figure class="figure <?php echo get_sub_field('full')? 'full' : 'content'?> <?php echo $padding ?>" style="background-image:url('<?php echo $src ?>')";>
     <img src="<?php echo $src ?>">
 </figure>
