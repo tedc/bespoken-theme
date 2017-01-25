22c9b8319143f8061807986f0b5f1249895f56f3
tedc <egrandinetti@gmail.com>
2017-01-25 19:37:34 +0100

    massive uploads

--- builder/intervista.php
+++ builder/intervista.php
@@ -1,20 +1,18 @@
-<div class="col-12 <?php echo get_sub_field('colore_sfondo') ?>">
-    <div class="video<?php echo get_sub_field('full') ? ' full' : ' content row-lg' ?>">
-        <div class="container-player">
-            <img class="screenshot" src="<?php echo get_sub_field('video') ?>">
-            <div class="video-foreground">
-                <?php $oembed = get_sub_field('video_embed');
-                echo $oembed; ?>
-            </div>
-            <h3 class="title-video"><?php _e('La parola del cliente', 'bspkn') ?></h3>
-        <span class="clients">
-            <h3><?php the_sub_field('titolo') ?></h3>
-            <h4><?php the_sub_field('sottotitolo') ?></h4>
-        </span>
-            <?php if (get_sub_field('video_embed') != "") { ?>
-                <span class="icon-play play"></span>
-                <?php close('isModal=false;isMenu=false'); ?>
-            <?php } ?>
+<div class="video<?php echo get_sub_field('full') ? ' full' : ' content row-lg' ?> <?php echo get_sub_field('colore_sfondo') ?>">
+    <div class="container-player">
+        <img class="screenshot" src="<?php echo get_sub_field('video') ?>">
+        <div class="video-foreground">
+            <?php $oembed = get_sub_field('video_embed');
+            echo $oembed; ?>
         </div>
+        <h3 class="title-video"><?php _e('La parola del cliente', 'bspkn') ?></h3>
+    <span class="clients">
+        <h3><?php the_sub_field('titolo') ?></h3>
+        <h4><?php the_sub_field('sottotitolo') ?></h4>
+    </span>
+        <?php if (get_sub_field('video_embed') != "") { ?>
+            <span class="icon-play play"></span>
+            <?php close('isModal=false;isMenu=false'); ?>
+        <?php } ?>
     </div>
 </div>
--- builder/riga.php
+++ builder/riga.php
@@ -1,5 +1,7 @@
+<div class="grid-12">
 <?php if (have_rows('colonna')) : ?>
     <?php while (have_rows('colonna')) : the_row(); ?>
         <?php include(locate_template('builder/' . get_row_layout() . '.php', false, false)); ?>
     <?php endwhile; ?>
 <?php endif ?>
+</div>
