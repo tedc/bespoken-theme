955aaff3b473768da51ed757fb363c2d40e6d212
tedc <egrandinetti@gmail.com>
2017-01-25 18:36:14 +0100

    layout

--- extras/setup.php
+++ extras/setup.php
@@ -64,7 +64,7 @@ function column_acf_flexible_content_layout_title( $title, $field, $layout, $i )
 add_filter('acf/fields/flexible_content/layout_title/name=colonna', 'column_acf_flexible_content_layout_title', 10, 4);
 
 function builder_acf_flexible_content_layout_title( $title, $field, $layout, $i ) {
-	if($field['acf_fc_layout'] === 'riga') :
+	if($layout === 'riga') :
 		$tite = $title . ':';
 		if(get_sub_field('colonna')) : foreach(get_sub_field('colonna') as $row) :
 		if($row['acf_fc_layout'] ===  'testo') {
