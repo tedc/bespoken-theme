0e5bce76a57e9fa7647e20e13a1b7cdcb034e5b5
tedc <egrandinetti@gmail.com>
2017-01-25 17:03:19 +0100

    video & slider

--- builder/contenuto/slider.php
+++ builder/contenuto/slider.php
@@ -38,8 +38,19 @@ endif;?>
                     <?php 
                     $n_page = 0;
                     foreach ($images as $image): ?>
-                        <?php $style = ' style="background-image:url('.wp_get_attachment_image_url( $image['id'], 'large' ).')"'; ?>         
-                        <div class="slider-item" ng-class="{current:pos==<?php echo $n_page ?>}"<?php echo $style; ?>>
+                        <?php $style = ' style="background-image:url('.wp_get_attachment_image_url( $image['id'], 'full' ).')"'; ?>         
+                        <div class="slider-item" ng-class="{current:pos==<?php echo $n_page ?>}"<?php echo $style; ?> id="slide_<?php echo $row; ?>_<?php $n; ?>">
+                            <style type="text/css">
+                                #slide_<?php echo $row; ?>_<?php $n; ?> {
+                                    background-image:url('.wp_get_attachment_image_url( $image['id'], 'medium' ).');
+                                }
+                                @media screen and (min-width: <?php echo 640/16; ?>em) {
+                                    background-image:url(<?php echo wp_get_attachment_image_url( $image['id'], 'large' ); ?>);
+                                }
+                                @media screen and (min-width: <?php echo 850/16; ?>em) {
+                                    background-image:url(<?php echo wp_get_attachment_image_url( $image['id'], 'full' ); ?>);
+                                }
+                            </style>
                             <figure><?php echo wp_get_attachment_image($image["id"], "large"); ?></figure>
                         </div>
                         <?php $n_page++; endforeach; ?>
