fb35db7c866e391b5af82ddbb127ea6876310923
tedc <egrandinetti@gmail.com>
2017-01-25 09:45:28 +0100

    menu

--- templates/modal-menu.php
+++ templates/modal-menu.php
@@ -3,8 +3,8 @@
 		<div class="gradient"></div>
 		<?php close('isModal=false;isMenu=false'); ?>
 		<?php
-			wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav',]);
-			wp_nav_menu(['theme_location' => 'service_navigation', 'menu_class' => 'nav', 'items_wrap' => '<ul id="%1$s" class="%2$s"><li class="title">'.__('I nostri servizi').'</li>%3$s</ul>']);
+			wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav', 'menu_id' => 'main-menu']);
+			wp_nav_menu(['theme_location' => 'service_navigation', 'menu_class' => 'nav', 'items_wrap' => '<ul id="%1$s" class="%2$s"><li class="title">'.__('I nostri servizi').'</li>%3$s</ul>', 'menu_id' => 'service-menu']);
 		?>
 		</div>
 </div>
