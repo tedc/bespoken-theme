c90d2807e81cb68d46fb8fbf9d6dcdac94f50fce
tedc <egrandinetti@gmail.com>
2017-01-25 17:46:53 +0100

    slider and builder

--- extras/setup.php
+++ extras/setup.php
@@ -19,5 +19,50 @@ function ng_app($html) {
 add_filter( 'language_attributes', 'ng_app', 100 );
 
 register_nav_menus([
-    'service_navigation' => __('Servizi', 'bspkn')
-  ]);
+	'service_navigation' => __('Servizi', 'bspkn')
+]);
+
+function my_acf_flexible_content_layout_title( $title, $field, $layout, $i ) {
+	$title = '<h4>' . $title . ':'
+
+	if((have_rows('contenuto')) : while(have_rows('contenuto')) : the_row();
+		if(get_row_layout() == 'testo') {
+			if(!get_sub_field('titolo_precompilato') && trim(get_sub_field('titolo'))!='') {
+				$title .= ' '.get_sub_field('titolo_precompilato').'</h4>';
+			} else {
+				$title .= ' '.get_sub_field('titolo').'</h4>';
+			}
+		} 
+		if(get_row_layout() == 'immagine') {
+			$img = get_sub_field('immagine')['sizes']['thumbnail'];
+			$title .= ' <div class="thumbnail"><<img src="'.$img.'" style="height:36px" /></div>';
+		}
+		if(get_row_layout() == 'immagine') {
+			$img = get_sub_field('immagine')['sizes']['thumbnail'];
+			$title .= ' <div class="thumbnail"><img src="'.$img.'" style="height:36px" /></div>';
+		}
+		if(get_row_layout() == 'slider') {
+			if (get_sub_field('tipologia') == 'immagini') {
+                $image = get_sub_field('galleria_immagini')[0];
+                $src = wp_get_attachment_image_url( $image['id'], 'thumbnail' );
+                $title .= ' <div class="thumbnail"><img src="'.$src.'" style="height:36px" /></div>';
+			}
+			if (get_sub_field('tipologia') == 'testo') {
+				$title .= ' Galleria di testo (ie. Manifesto)'.'</h4>';
+			}
+		}
+		if(get_row_layout() == 'video') {
+			$file = preg_replace('/\\.[^.\\s]{3,4}$/', '', get_sub_field('file'));
+			$title .= ' <div class="thumbnail"><img src="'.$file.'.jpg" style="height:36px" /></div>';
+		}	
+		if(get_row_layout() == 'citazoine') {
+			$title .= ' Citazione'.'</h4>';
+		}
+	endwhile; endif;
+	// return
+	return $title;
+	
+}
+
+// name
+add_filter('acf/fields/flexible_content/layout_title/name=colonna', 'my_acf_flexible_content_layout_title', 10, 4);
