f91992cebd7ba1bba52b9efd68361a7993efae7d
tedc <egrandinetti@gmail.com>
2017-01-25 18:28:32 +0100

    layout title

--- extras/setup.php
+++ extras/setup.php
@@ -64,23 +64,24 @@ function column_acf_flexible_content_layout_title( $title, $field, $layout, $i )
 add_filter('acf/fields/flexible_content/layout_title/name=colonna', 'column_acf_flexible_content_layout_title', 10, 4);
 
 function builder_acf_flexible_content_layout_title( $title, $field, $layout, $i ) {
-	if(get_row_layout() === 'riga') {
+	function builder_acf_flexible_content_layout_title( $title, $field, $layout, $i ) {
+	if($field['acf_fc_layout'] === 'riga') {
 		$tite = $title . ':';
-		if(have_rows('colonna')) : $row = 0; while(have_rows('colonna')) : the_row();
-			if(get_row_layout() ===  'testo') {
-			if(get_sub_field('testo')[0]['titolo_precompilato'] && trim(get_sub_field('testo')[0]['titolo'])=='') {
-				$title .= ' '.get_sub_field('testo')[0]['titolo_precompilato'];
+		if(get_sub_field('colonna')) : foreach(get_sub_field('colonna') as $row);
+			if($row['acf_fc_layout'] ===  'testo') {
+			if($row['testo'][0]['titolo_precompilato'] && trim($row['testo'][[0]['titolo'])=='') {
+				$title .= ' '.$row['testo'[[0]['titolo_precompilato'];
 			} else {
-				$title .= ' '.get_sub_field('testo')[0]['titolo'];
+				$title .= ' '.$row['testo'][0]['titolo'];
 			}
 		} 
-		if(get_row_layout() === 'immagine') {
-			$img = get_sub_field('immagine')['sizes']['thumbnail'];
+		if($row['acf_fc_layout'] === 'immagine') {
+			$img = $row['immagine']['sizes']['thumbnail'];
 			$title .= ' <div class="thumbnail" style="display:inline-block;"><img src="'.$img.'" style="height:36px" /></div>';
 		}
-		if(get_row_layout() === 'slider') {
-			if (get_sub_field('tipologia') == 'immagini') {
-                $image = get_sub_field('galleria_immagini')[0];
+		if($row['acf_fc_layout'] === 'slider') {
+			if ($row['tipologia'] == 'immagini') {
+                $image = $row['galleria_immagini'][0];
                 $src = wp_get_attachment_image_url( $image['id'], 'thumbnail' );
                 $title .= ' <div class="thumbnail" style="display:inline-block;"><img src="'.$src.'" style="height:36px" /></div>';
 			}
@@ -88,14 +89,14 @@ function builder_acf_flexible_content_layout_title( $title, $field, $layout, $i 
 				$title .= ' Galleria di testo (ie. Manifesto)';
 			}
 		}
-		if(get_row_layout() === 'video') {
-			$file = preg_replace('/\\.[^.\\s]{3,4}$/', '', get_sub_field('file'));
+		if($row['acf_fc_layout'] === 'video') {
+			$file = preg_replace('/\\.[^.\\s]{3,4}$/', '', $row('file'));
 			$title .= ' <div class="thumbnail" style="display:inline-block;"><img src="'.$file.'.jpg" style="height:36px" /></div>';
 		}	
-		if(get_row_layout() === 'citazoine') {
+		if($row['acf_fc_layout'] === 'citazoine') {
 			$title .= ' Citazione';
 		}
-		$row++; endwhile; endif;
+		endforeach; endif;
 	}
 
 	return $title;
