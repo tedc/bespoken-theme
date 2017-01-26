d50799281c90c2bdb326364660e85683c055af8c
Mattia <guzzi.mattia@gmail.com>
2017-01-26 11:18:13 +0100

    no message

--- builder/contenuto/slider.php
+++ builder/contenuto/slider.php
@@ -60,7 +60,7 @@ endif;?>
                 <?php if (have_rows('galleria_testo')): ?>
                     <?php $n_page = 0;
                     while (have_rows('galleria_testo')) : the_row(); ?>
-                        <div class="slider-item <?php echo get_sub_field('posizione_verticale') ?>" ng-class="{current:pos==<?php echo $n_page ?>}">
+                        <div class="slider-item" ng-class="{current:pos==<?php echo $n_page ?>}">
                             <?php the_sub_field('pagine'); ?>
                         </div>
                         <?php $n_page++; endwhile ?>
