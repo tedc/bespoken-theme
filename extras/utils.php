3aee97177b187a2b58e5526816cbf45523404cd1
tedc <egrandinetti@gmail.com>
2017-01-26 18:16:40 +0100

    sm

--- extras/utils.php
+++ extras/utils.php
@@ -9,7 +9,7 @@
 		?>
 		<div<?php if($q->found_posts > 1) : ?> ng-carousel items="<?php echo $el; ?>" max="<?php echo $q->found_posts; ?>" mousewheel="<?php echo $mw; ?>"<?php endif; ?> class="carousel<?php echo ($l) ? ' closing' : ''; ?>"<?php echo ($l) ? ' id="last-related"' : ''; ?>>
 			<?php $sm = ($l) ? ' ng-sm trigger-element="last-related" to="{y : "0%"}" trigger-hook="onEnter"' : ''; ?>
-			<div class="carousel-container"<?php $sm; ?>>
+			<div class="carousel-container"<?php echo $sm; ?>>
 				<div class="carousel-wrapper">
 					<?php while($q->have_posts()) : $q->the_post(); ?>
 					<?php get_template_part( 'templates/content', 'related' ); ?>
