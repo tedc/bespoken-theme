6f3db045e2f7b1c23e9843b000cf05bb17646d16
Mattia <guzzi.mattia@gmail.com>
2017-01-26 14:41:52 +0100

    no message

--- templates/page-header.php
+++ templates/page-header.php
@@ -4,8 +4,8 @@
     <?php get_template_part('templates/header', get_field('kind')) ?>
     <div class="container-text-header">
         <div class="text-header">
-            <?php if (get_field('titolo')) : ?>
-            <h1 class="title"><?php the_field('titolo'); ?></h1>
+            <?php if (get_field('titolo_testata')) : ?>
+            <h1 class="title"><?php the_field('titolo_testata'); ?></h1>
             <?php endif; ?>
             <?php if (get_field('sottotitolo')) : ?>
                 <h2 class="subtitle"><?php the_field('sottotitolo'); ?></h2>
