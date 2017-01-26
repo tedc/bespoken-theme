50b5b2b505db9e6f2bc7fefb74e2ee35de3e440a
Mattia <guzzi.mattia@gmail.com>
2017-01-26 12:51:13 +0100

    no message

--- builder/contenuto/testo.php
+++ builder/contenuto/testo.php
@@ -1,23 +1,25 @@
-<?php while ( have_rows('testo') ) : the_row(); ?>
+<?php while (have_rows('testo')) : the_row(); ?>
 
-<?php if (get_sub_field('spaziatura')== '') :
-    $padding = 'row-lg';
-elseif (get_sub_field('spaziatura') == '0'):
-    $padding = get_sub_field('grandezza_spaziatura');
-elseif (get_sub_field('spaziatura') == '1'):
-    $padding = get_sub_field('grandezza_spaziatura_sopra');
-elseif (get_sub_field('spaziatura') == '2'):
-    $padding = get_sub_field('grandezza_spaziatura_sotto');
-endif;?>
+    <?php if (get_sub_field('spaziatura') == '') :
+        $padding = 'row-lg';
+    elseif (get_sub_field('spaziatura') == '0'):
+        $padding = get_sub_field('grandezza_spaziatura');
+    elseif (get_sub_field('spaziatura') == '1'):
+        $padding = get_sub_field('grandezza_spaziatura_sopra');
+    elseif (get_sub_field('spaziatura') == '2'):
+        $padding = get_sub_field('grandezza_spaziatura_sotto');
+    endif; ?>
 
-<div class="content <?php echo $padding ?>">
-    <?php $align = ((get_sub_field('allineamento_testo'))); ?>
-    <?php if(!get_sub_field('titolo_precompilato') && trim(get_sub_field('titolo'))!='') :?>
+    <div class="content <?php echo $padding ?>">
+        <?php $align = ((get_sub_field('allineamento_testo'))); ?>
+        <?php if (!get_sub_field('titolo_precompilato') && trim(get_sub_field('titolo')) != '') : ?>
 
-        <h3 class="title <?php echo $align ?> <?php echo (get_sub_field('enfasi_titolo')? ' emphasis' : '' )  ?> <?php the_sub_field('grandezza_titolo')?>"><?php the_sub_field('titolo') ?> </h3>
+            <h3 class="title <?php echo $align ?> <?php echo(get_sub_field('enfasi_titolo') ? ' emphasis' : '') ?> <?php the_sub_field('grandezza_titolo') ?>"><?php the_sub_field('titolo') ?> </h3>
         <?php else : ?>
-        <h2 class="title title-minus emphasis"><em><?php the_sub_field('titolo_precompilato');?></em></h2>
+            <h2 class="title title-minus emphasis"><em><?php the_sub_field('titolo_precompilato'); ?></em></h2>
         <?php endif ?>
-<div class="description <?php echo $align ?>"><?php the_sub_field('contenuto') ?></div>
+        <?php if (!get_sub_field('contenuto')) : ?>
+            <div class="description <?php echo $align ?>"><?php the_sub_field('contenuto') ?></div>
+        <?php endif ?>
     </div>
 <?php endwhile ?>
