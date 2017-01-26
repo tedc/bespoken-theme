d2e9316b9a3b8a4674cc80bb72348c2dc481e2ea
tedc <egrandinetti@gmail.com>
2017-01-26 15:48:07 +0100

    video

--- builder/intervista.php
+++ builder/intervista.php
@@ -6,10 +6,10 @@
             echo $oembed; ?>
         </div>
         <h3 class="title-video"><?php _e('La parola del cliente', 'bspkn') ?></h3>
-    <span class="clients">
-        <h3><?php the_sub_field('titolo') ?></h3>
-        <h4><?php the_sub_field('sottotitolo') ?></h4>
-    </span>
+        <div class="clients row">
+            <h3><?php the_sub_field('titolo') ?></h3>
+            <h4><?php the_sub_field('sottotitolo') ?></h4>
+        </div>
         <?php if (get_sub_field('video_embed') != "") { ?>
             <span class="icon-play play"></span>
             <?php close('isModal=false;isMenu=false'); ?>
