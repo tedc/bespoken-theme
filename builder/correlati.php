59cf420da3888f23a3fe00784646d1a903a81eb5
tedc <egrandinetti@gmail.com>
2017-01-27 17:50:48 +0100

    row md btm

--- builder/correlati.php
+++ builder/correlati.php
@@ -27,7 +27,7 @@
 	);
 	if(get_post_type() == 'servizi') {
 		?>
-	<div class="grid-12 alternate row-lg">
+	<div class="grid-12 alternate row-md-btm">
 		<div class="col-12 aligncenter">
 			<h3 class="title title-normal-important"><?php _e('Le attività da svolgere', 'bspkn'); ?></h3>
 		</div>
