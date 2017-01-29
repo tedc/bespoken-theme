8010445259d5af0253ac438e64094177cbef6394
tedc <egrandinetti@gmail.com>
2017-01-29 17:32:42 +0100

    breadcrumb

--- case-study.php
+++
@@ -1,5 +0,0 @@
-<?php
-/**
- * Template Name: Case Study
- */
-?>
---
+++ templates/about-header.php
@@ -0,0 +1,16 @@
+<?php use Roots\Sage\Titles; ?>
+<?php get_template_part('templates/breadcrumbs'); ?>
+<header class="page-header about-header" id="header">
+    <div class="gradient" ng-sm trigger-element="#header" trigger-hook="onLeave" to="{y : '10%'}" duration="120%"></div>
+    <div class="container-text-header">
+        <div class="text-header">
+            <h1 class="title">
+                <?php _e('Siamo', 'bspkn'); ?>
+                <strong class="words">
+                    <?php while(have_rows('words')) : the_row(); ?>
+                    <span class="word"><?php the_sub_field('word'); ?></span>
+                </strong>
+            </h1>
+        </div>
+    </div>
+</header>
