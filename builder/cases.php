9b294414d048c815aae70722bf1f7f1ad9ef9ab3
tedc <egrandinetti@gmail.com>
2017-01-29 18:28:24 +0100

    cases

--- builder/cases.php
+++ builder/cases.php
@@ -10,22 +10,26 @@ $query = new WP_Query(
 		);
 if(get_sub_field('case_kind') == 0) : ?>
 	<?php $colClass = get_sub_field('cols'); ?>
-	<div class="grid-12 row-lg alternate">
+	<div class="grid-12 row-lg alternate cases">
 		<?php 
 		
 		while($query->have_posts()) : $query->the_post(); ?>
-		<li <?php post_class('col-'.get_sub_field('cols content')); ?>">
+		<li <?php post_class('col-'.$colClass . ' content'); ?>">
 			<?php if(preg_match($pattern, get_field('icon', get_the_ID()))) : ?>
-                
+			<a href="<?php the_permalink(); ?>">
 			<?php 
                     $svg = file_get_contents(get_field('icon'));
                     $svg = str_replace('id', 'class="case-svg" id', $svg);
                     echo $svg;
                 ?>
-
+            </a>
             <?php else : ?>
-	            <img src="<?php the_field('icon', $post->ID) ?>" class="case-image">
-                <img src="<?php the_field('icon_white', $post->ID) ?>" class="case-image-overlay">
+            <figure class="case-figure">
+	          	<a href="<?php the_permalink(); ?>">
+		            <img src="<?php the_field('icon', $post->ID) ?>" class="case-image">
+	                <img src="<?php the_field('icon_white', $post->ID) ?>" class="case-image-overlay">
+                </a>
+             </figure>
             <?php endif; ?>
 		</li>
 		<?php endwhile; wp_reset_query(); ?>
