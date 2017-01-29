9c9017d95b327c857b09cea117a3e2314662e2fe
tedc <egrandinetti@gmail.com>
2017-01-29 21:46:09 +0100

    blog

--- index.php
+++ index.php
@@ -7,8 +7,8 @@
   <?php get_search_form(); ?>
 <?php endif; ?>
 
-<?php while (have_posts()) : the_post(); ?>
-  <?php get_template_part('templates/content', get_post_type() != 'post' ? get_post_type() : get_post_format()); ?>
-<?php endwhile; ?>
+<?php $count = 0; while (have_posts()) : the_post(); ?>
+  <?php include(locate_template('templates/content-'. get_post_type() != 'post' ? get_post_type() : get_post_format() .'.php', false, false)); ?>
+<?php $count++; endwhile; ?>
 
 <?php the_posts_navigation(); ?>
