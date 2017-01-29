fc2b9665d517ae5edf98cefe9e36b46f828eb1f4
tedc <egrandinetti@gmail.com>
2017-01-29 22:42:08 +0100

    blog

--- templates/content.php
+++ templates/content.php
@@ -1,10 +1,11 @@
 <?php $postClass = ($count%2 == 0) ? '' : 'alt'; ?>
 <article <?php post_class(); ?>>
-	<figure class="entry-figure">
-		<?php the_post_thumbnail(); ?>
+	<figure class="entry-figure" style="background-image: url(<?php the_post_thumbnail_url('large'); ?>)">
+		<?php the_post_thumbnail('large'); ?>
 	</figure>
-	<div class="content">
+	<div class="entry-content">
 		<header>
+			<?php the_category(', '); ?>
 			<h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 			<?php get_template_part('templates/entry-meta'); ?>
 		</header>
