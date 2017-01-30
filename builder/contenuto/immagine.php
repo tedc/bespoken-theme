bf37cbda069c57c9f27d87ffed0728c8a6124975
tedc <egrandinetti@gmail.com>
2017-01-30 09:33:20 +0100

    immagine

--- builder/contenuto/immagine.php
+++ builder/contenuto/immagine.php
@@ -20,7 +20,7 @@ if(preg_match($pattern, get_sub_field('immagine')['url'])) : ?>
 
 <?php else : ?>
 <figure class="figure <?php echo get_sub_field('full')? 'full' : 'content'?> <?php echo $padding ?>" id="image_<?php echo get_sub_field('immmagine')['ID']; ?>">
-	<img src="<?php echo get_sub_field('immagine')['url']; ?>" ng-sm from="{y : '-50%'}" to="{y : 50%}" duration="200%" trigger-hook="onEnter" trigger-eleme="#image_<?php echo get_sub_field('immmagine')['ID']; ?>">
+	<img src="<?php echo get_sub_field('immagine')['url']; ?>" ng-sm from="{y : '-50%'}" to="{y : '50%'}" duration="200%" trigger-hook="onEnter" trigger-eleme="#image_<?php echo get_sub_field('immmagine')['ID']; ?>">
 </figure>
 
 <?php endif; ?>
