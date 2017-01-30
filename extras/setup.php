bc8d09f8c24b4df94d1fc3f5146d4f6cdd182150
Mattia <guzzi.mattia@gmail.com>
2017-01-30 11:19:05 +0100

    no message

--- extras/setup.php
+++ extras/setup.php
@@ -109,4 +109,4 @@ function cc_mime_types($mimes) {
   $mimes['svg'] = 'image/svg+xml';
   return $mimes;
 }
-add_filter('upload_mimes', 'cc_mime_types',1,1);
+add_filter('upload_mimes', 'cc_mime_types');
