6bf9597d11792edd8e004a9c052cc2776e98196d
tedc <egrandinetti@gmail.com>
2017-01-30 09:52:54 +0100

    slow anim

--- builder/contenuto/immagine.php
+++ builder/contenuto/immagine.php
@@ -19,7 +19,7 @@ if(preg_match($pattern, get_sub_field('immagine')['url'])) : ?>
 </figure>
 
 <?php else : ?>
-<figure class="figure <?php echo get_sub_field('full')? 'full' : 'content'?> <?php echo $padding ?>"<?php if(get_sub_field('full')): ?> style="background-image:url(<?php echo get_sub_field('immagine')['url']; ?>);" ng-sm from="{'background-position' : '50% -200px'}" to="{'background-position' : '50% 0'}" duration="200" trigger-hook="onEnter"<?php endif; ?>>
+<figure class="figure <?php echo get_sub_field('full')? 'full' : 'content'?> <?php echo $padding ?>"<?php if(get_sub_field('full')): ?> style="background-image:url(<?php echo get_sub_field('immagine')['url']; ?>);" ng-sm from="{'background-position' : '50% -20px'}" to="{'background-position' : '50% 20px'}" duration="400%" trigger-hook="onEnter"<?php endif; ?>>
 	<img src="<?php echo get_sub_field('immagine')['url']; ?>">
 </figure>
 
