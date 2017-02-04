6682aeac0b451ba278cfa5eb95655836ce224677
tedc <egrandinetti@gmail.com>
2017-02-04 12:16:16 +0100

    style home

--- front-page.php
+++ front-page.php
@@ -1,5 +1,5 @@
 <section class="home-page" ng-home ng-class="{scrolled:isScrolled}">
-	<header class="page-header" id="header" ng-mouse-wheel-down="scrollWheel()">
+	<header class="page-header" id="header">
 		<div class="gradient"></div>
 		<h2 class="home-title">
 			<span class="home-title-row">
