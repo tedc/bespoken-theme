55a9d0af322245e9e2b03481db4d6313a0e3e20b
tedc <egrandinetti@gmail.com>
2017-02-05 12:26:36 +0100

    front page

--- front-page.php
+++ front-page.php
@@ -1,7 +1,7 @@
-<section class="home-page" ng-home ng-class="{scrolled:isScrolled}">
+<section class="home-page">
 	<header class="page-header" id="header">
 		<div class="gradient"></div>
-		<h2 class="home-title">
+		<h2 class="home-title" ng-sm to="{y : 120, opacity : 0}" duration="100%" trigger-hook="0.1">
 			<span class="home-title-row">
 				<?php echo strip_tags(get_field('first_row'), '<a>'); ?>
 			</span><br />
