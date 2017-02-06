f576b7b5793e36f6dd4acf2f1f03fc77c141ecca
tedc <egrandinetti@gmail.com>
2017-02-06 09:04:56 +0100

    parallax

--- templates/page-header.php
+++ templates/page-header.php
@@ -2,7 +2,7 @@
 <?php get_template_part('templates/breadcrumbs'); ?>
 <header class="page-header" id="header">
     <?php get_template_part('templates/header', get_field('kind')) ?>
-    <div class="container-text-header row-lg" ng-sm trigger-element=".main" trigger-hook="onLeave" duration="120%" to="{y : '20%'}">
+    <div class="container-text-header row-lg" ng-sm trigger-element=".main" trigger-hook="onLeave" duration="120%" to="{y : '50%'}">
         <div class="text-header">
             <?php if(get_field('titolo_alternativo')) :?>
             <?php if (get_field('titolo_testata')) : ?>
