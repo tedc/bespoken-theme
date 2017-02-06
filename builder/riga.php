226275787382bad40136fda848c4a7121fda71ac
tedc <egrandinetti@gmail.com>
2017-02-06 12:04:15 +0100

    gradient

--- builder/riga.php
+++ builder/riga.php
@@ -1,4 +1,5 @@
-<div class="grid-12">
+<?php $gridClass= (get_sub_field('col_inverted')) ? 'grid-12 grid-inverted' : ''; ?>
+<div class="<?php echo $gridClass; ?>">
 <?php if (have_rows('colonna')) : ?>
     <?php while (have_rows('colonna')) : the_row(); ?>
         <?php include(locate_template('builder/' . get_row_layout() . '.php', false, false)); ?>
