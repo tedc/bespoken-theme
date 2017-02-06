2f7d9aa7d16e33c100dc19b02ec959b033fafe04
tedc <egrandinetti@gmail.com>
2017-02-06 21:16:25 +0100

    cases

--- builder/cases.php
+++ builder/cases.php
@@ -38,11 +38,44 @@ if(get_sub_field('case_kind') == 0) : ?>
 <?php else : ?>
 <div class="video-carousel video bg-dark" ng-carousel items="1" max="<?php echo $query->found_posts; ?>">
 	<div class="carousel-wrapper">
-	<?php 
-		
-		while($query->have_posts()) : $query->the_post(); ?>
+	<?php while($query->have_posts()) : $query->the_post(); ?>
 		<div class="carouse-item">
-		<?php var_dump(get_field('builder')); ?>
+		<?php while(have_rows('builder')) : the_row(); ?>
+		<?php if(get_row_layout() == 'intervista') : ?>
+		<?php
+            $pattern = '/embed\/(\w+)\?\w+/';
+            $oembed = get_sub_field('video_embed');
+            $iframe = preg_match($pattern, $oembed, $matches);
+		?>	
+		<div class="container-player" ng-player player="<?php echo $matches[1]; ?>">
+        <img class="screenshot" src="<?php the_sub_field('video'); ?>">
+        <?php echo $row; ?>"><?php _e('La parola del cliente', 'bspkn') ?></h3>
+        <div class="clients row">
+            <h4 class="name"><?php the_sub_field('titolo') ?></h4>
+            <h5 class="company"><?php the_sub_field('sottotitolo') ?></h5>
+        </div>
+        <?php if (get_sub_field('video_embed') != "") { ?>       
+        <div class="video-foreground" style="background-image: url(<?php the_sub_field('video'); ?>)" ng-class="{visible:isStarted}">
+            <youtube-video id="player_<?php echo $row; ?>" video-id="video.id" player-vars="video.vars" player="video.player" ng-class="{visible: isPlaying}"></youtube-video>
+            <?php close('isPlaying=false;isStarted=false;video.player.stopVideo()'); ?> 
+            <div class="controls">
+                <div class="buttons">
+                    <span class="play-pause" ng-click="playPause(video.player)" ng-class="{playing: !isPaused}"></span>
+                    <span class="fs" title="<?php _e('Full screen', 'bspkn'); ?>" ng-click="fulScreen('player_<?php echo $row; ?>')"></span>
+                </div>
+                <div class="status-bar" ng-click="skipTo($event, video.player)">
+                    <div class="progress-bar"></div>
+                    <div class="progress-mask">
+                        <span class="time" ng-bind-html="time" ng-class="{invert: isHalf}"></span>
+                    </div>
+                </div>
+            </div>
+        </div> 
+        <span class="play" ng-click="isStarted=true;video.player.playVideo()" ng-class="{visible:isReady}"></span>        
+        <?php } ?>
+    	</div>
+		<?php endif; ?>
+		<?php endwhile;?>
 		</div>
 		<?php endwhile; wp_reset_query();  ?>
 		<?php if($query->found_posts > 1) : ?>
