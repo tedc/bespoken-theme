ced80db2b2319d7391e5bb480d48b8d43c6bc417
tedc <egrandinetti@gmail.com>
2017-02-06 12:18:59 +0100

    varie

--- builder/riga.php
+++ builder/riga.php
@@ -1,4 +1,4 @@
-<?php $gridClass= (get_sub_field('col_inverted')) ? 'grid-12 grid-inverted' : ''; ?>
+<?php $gridClass= (get_sub_field('col_inverted')) ? 'grid-12 grid-inverted' : 'grid-12'; ?>
 <div class="<?php echo $gridClass; ?>">
 <?php if (have_rows('colonna')) : ?>
     <?php while (have_rows('colonna')) : the_row(); ?>
