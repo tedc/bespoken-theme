8be91f6697cc681254f368e05f4b50f739a7a2c0
tedc <egrandinetti@gmail.com>
2017-02-07 12:48:38 +0100

    above the fold

---
+++ extras/styles.php
@@ -0,0 +1,11 @@
+<?php
+function footer_css() {
+	wp_enqueue_style('sage/css', Assets\asset_path('styles/main.css'), false, null);
+}
+add_action('wp_footer', 'footer_css', 999);
+function head_css() { ?>
+	<style>
+		body { visibility: hidden}
+	</style>
+<? }
+add_action('wp_head', 'head_css');
--- lib/setup.php
+++ lib/setup.php
@@ -95,7 +95,7 @@ function display_sidebar() {
  * Theme assets
  */
 function assets() {
-  wp_enqueue_style('sage/css', Assets\asset_path('styles/main.css'), false, null);
+  //wp_enqueue_style('sage/css', Assets\asset_path('styles/main.css'), false, null);
   wp_enqueue_script( 'lib', Assets\asset_path('scripts/lib.js'), null, null, true );
   wp_enqueue_script('sage/js', Assets\asset_path('scripts/main.js'), 'lib', null, true);
 }
