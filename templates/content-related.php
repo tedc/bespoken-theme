3f9fca6b1ce76c71c1faf6a9a60a13b7ea72ff24
tedc <egrandinetti@gmail.com>
2017-02-07 13:52:45 +0100

    is mobile

--- templates/content-related.php
+++ templates/content-related.php
@@ -1,7 +1,7 @@
 <?php 
 $pattern = '/[\w\-]+\.(svg)/'; ?>
 <div class="carousel-item">
-	<div class="cover"<?php if(is_mobble()) : ?> ng-attr-style="background-image: url(<?php the_post_thumbnail_url('large'); ?>)"<?php else : ?> ng-attr-style="background-image: url(<?php the_post_thumbnail_url('slider_images'); ?>)"<?php endif; ?>></div>
+	<div class="cover"<?php if(is_mobile()) : ?> ng-attr-style="background-image: url(<?php the_post_thumbnail_url('large'); ?>)"<?php else : ?> ng-attr-style="background-image: url(<?php the_post_thumbnail_url('slider_images'); ?>)"<?php endif; ?>></div>
 	<div class="carousel-item-content row-lg">
 		<h2 class="title"><?php the_title(); ?></h2>
 		<span class="btn"><span class="btn-text"><?php _e('Scopri', 'bspkn'); ?></span></span>
