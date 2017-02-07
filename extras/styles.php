6cc3ccddf51fe3639b204fa69c3401bb78816aeb
tedc <egrandinetti@gmail.com>
2017-02-07 13:24:13 +0100

    slider_images

--- extras/styles.php
+++ extras/styles.php
@@ -3,10 +3,15 @@ use Roots\Sage\Assets;
 function footer_css() {
 	wp_enqueue_style('sage/css', Assets\asset_path('styles/main.css'), false, null);
 }
-add_action('wp_footer', 'footer_css', 999);
+//add_action('wp_footer', 'footer_css', 999);
 function head_css() { ?>
 	<style>
 		body { visibility: hidden}
 	</style>
 <? }
-add_action('wp_head', 'head_css');
+//add_action('wp_head', 'head_css');
+
+function crop_service_images() {
+	add_image_size('slider_images', 957, 957, true);
+}
+add_action('after_setup_theme', 'crop_service_images');
