a687ff2201e0be97e3f4d976e10827d234e49ccf
tedc <egrandinetti@gmail.com>
2017-02-07 13:00:05 +0100

    media

--- templates/content-related.php
+++ templates/content-related.php
@@ -1,7 +1,7 @@
 <?php 
 $pattern = '/[\w\-]+\.(svg)/'; ?>
 <div class="carousel-item">
-	<div class="cover" style="background-image: url(<?php the_post_thumbnail_url('full'); ?>)"></div>
+	<div class="cover" style="background-image: url(<?php the_post_thumbnail_url('large'); ?>)"></div>
 	<div class="carousel-item-content row-lg">
 		<h2 class="title"><?php the_title(); ?></h2>
 		<span class="btn"><span class="btn-text"><?php _e('Scopri', 'bspkn'); ?></span></span>
