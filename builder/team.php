bdcbbd398ba24de3a7c60fadb04696f2e4421c1a
tedc <egrandinetti@gmail.com>
2017-02-14 14:09:10 +0100

    dump user

--- builder/team.php
+++ builder/team.php
@@ -13,11 +13,12 @@
             <div class="col-<?php the_sub_field("n_cols") ?> user-col<?php echo $colClass; ?>" data-position="<?php echo $i ?>">
                 <figure class="user-image">
                     <?php
-                    $imgId = get_field('avatar', 'user_'.$user->ID);
-                    if($imgId) {
-                        $img = wp_get_attachment_image($imgId, 'large', false, array('class' => 'avatar')); 
-                        echo $img;
-                    }
+                    var_dump($user);
+                    // $imgId = get_field('avatar', 'user_'.$user->ID);
+                    // if($imgId) {
+                    //     $img = wp_get_attachment_image($imgId, 'large', false, array('class' => 'avatar')); 
+                    //     echo $img;
+                    // }
                     ?>
                 </figure>
                 <div class="mask">
