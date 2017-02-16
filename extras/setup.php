f2de499c3e871e433b41aca4ca546b58832e2495
tedc <egrandinetti@gmail.com>
2017-02-16 15:59:49 +0100

    prova permalink servizi

--- extras/setup.php
+++ extras/setup.php
@@ -147,3 +147,28 @@ function favicon() {
 <?php }
 
 add_action( 'wp_head', 'favicon' );
+
+function na_remove_slug( $post_link, $post, $leavename ) {
+
+    if ( 'servizi' != $post->post_type || 'publish' != $post->post_status ) {
+        return $post_link;
+    }
+
+    $post_link = str_replace( '/' . $post->post_type . '/', '/', $post_link );
+
+    return $post_link;
+}
+add_filter( 'post_type_link', 'na_remove_slug', 10, 3 );
+
+
+function na_parse_request( $query ) {
+
+    if ( ! $query->is_main_query() || 2 != count( $query->query ) || ! isset( $query->query['page'] ) ) {
+        return;
+    }
+
+    if ( ! empty( $query->query['name'] ) ) {
+        $query->set( 'post_type', array( 'post', 'servizi', 'page', 'lavori' ) );
+    }
+}
+add_action( 'pre_get_posts', 'na_parse_request' );
