4bbc37e87fb397b01860a151738c36547b6bea62
tedc <egrandinetti@gmail.com>
2017-02-16 22:35:17 +0100

    permalink

--- builder/form-contact.php
+++ builder/form-contact.php
@@ -1,3 +1,3 @@
-<form class="form" name="contactForm" ng-submit="onSubmit(contactForm.$valid, '<?php echo $current_url; ?>')" ng-form>
+<form class="form" name="contactForm" ng-submit="onSubmit(contactForm.$valid, '<?php the_permalink(); ?>')" ng-form>
 	<?php get_template_part( 'templates/form', 'fields' ); ?>
 </form>
